24ef77c193997bfc44049929cd7dfbe948792224
klei1984 <53688147+klei1984@users.noreply.github.com>
2021-07-05 20:42:40 +0200

    Reimplemented the smart string module
    The original module was called strobj. The smart string module
    implements reference counting, automatic garbage collection and many
    typical string manipulation functions. The container for basic C strings
    can be resized if needed. The growth factor is typically 64 characters
    (bytes).
    
    The involved classes are much more resource efficient than the standard
    String class implementation from the Watcom C/C++ 10.5 compiler.
    
    The smart string class methods are referenced in hundreds of places and
    it makes no sense to integrate the reimplementation into the original
    code base, but the module is necessary for other smart classes like the
    TextFileObject or UNITINFO.
    
    The original SmartString::Substr method is very odd. There is a single
    call site in the original code base and it passes the destination
    argument by its address without constructing the object first. This
    implies that the argument is not a reference, but a pointer to a class
    instance. Then in the Substr method itself, the destination is set to
    the result of the call to the SmartString class's copy constructor. The
    copy constructor was identified via the compiler generated RTTI data.
    The class overloads the assignment operator, but that operator is not
    called at the place where the copy constructor assigns the object’s
    reference to the dereferenced destination argument. The locally copy
    constructed object is not destructed as the object goes out of scope
    either.
    
    So the caller had a pointer to a not yet allocated object, and the copy
    constructor inside the callee is executed without first allocating
    memory for the object to be constructed! The placement new operator was
    not called either and the object was not destructed when it should have
    gone out of scope.
    
    I spent several hours trying to find a way to exactly replicate the
    original code, but could not figure it out…
    
    The API also works in a strange way regardless of the above oddities.
    The length of the substring to be copied is handled inconsistently
    inside the method. In most cases the method copies one more character
    then one would expect. E.g. if the API call is string.Substr(position=0,
    length=12), then the resulting substring will have 13 characters from
    the original string plus the nul C string delimiter.

---
+++ src/smartstring.hpp
@@ -0,0 +1,80 @@
+/* Copyright (c) 2021 M.A.X. Port Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SMARTSTRING_HPP
+#define SMARTSTRING_HPP
+
+class StringObject {
+    friend class SmartString;
+
+    char* buffer;
+    unsigned short size;
+    unsigned short length;
+    unsigned int reference_count;
+
+public:
+    StringObject(unsigned short size);
+    StringObject(const char* cstring);
+    StringObject(const StringObject& other);
+    ~StringObject();
+
+    void Resize(unsigned short size, bool keep);
+    unsigned short GetLength() const;
+    unsigned short GetSize() const;
+    char* GetCStr() const;
+    void SetLength(unsigned short length);
+};
+
+class SmartString {
+    StringObject* object_pointer;
+
+    void Increment() const;
+    void Decrement();
+    void Resize(unsigned short size, bool keep);
+    void Copy();
+    bool IsLastReference();
+    static unsigned short CalcOptimalCapacity(unsigned short needed_capacity);
+
+public:
+    SmartString();
+    SmartString(unsigned short size);
+    SmartString(const char* cstring);
+    SmartString(const SmartString& other);
+    ~SmartString();
+
+    char* GetCStr() const;
+    SmartString& Substr(SmartString* destination, unsigned short position, unsigned short length);
+    char& operator[](unsigned short position);
+    SmartString& operator+=(SmartString const& other);
+    SmartString& operator+=(const char* cstring);
+    SmartString& operator+=(const char character);
+    char* StrStr(const char* cstring) const;
+    char* StrStr(const SmartString& other) const;
+    SmartString& Clear();
+    SmartString& Toupper();
+    SmartString& Tolower();
+    SmartString& Vsprintf(unsigned short size, const char* format, ...);
+    int Strcmp(const char* cstring, bool case_sensitive) const;
+    SmartString& operator=(SmartString const& rhs);
+    SmartString& operator=(char const* rhs);
+};
+
+#endif /* SMARTSTRING_HPP */
