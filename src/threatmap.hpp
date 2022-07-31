1ca8f546d9f18573ab64ed1d5b51fd4967794065
klei1984 <53688147+klei1984@users.noreply.github.com>
2022-07-31 23:32:19 +0200

    Reimplemented three more utility classes

---
+++ src/threatmap.hpp
@@ -0,0 +1,52 @@
+/* Copyright (c) 2022 M.A.X. Port Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef THREATMAP_HPP
+#define THREATMAP_HPP
+
+#include "unitinfo.hpp"
+
+class ThreatMap {
+    short team;
+    short id;
+    short **damage_potential_map;
+    short **shots_map;
+    Point dimension;
+    short armor;
+    unsigned char risk_level;
+    unsigned char caution_level;
+    unsigned char field_18;
+
+    void Deinit();
+
+public:
+    ThreatMap();
+    ~ThreatMap();
+
+    void Init();
+
+    unsigned short GetRiskLevel(ResourceID unit_type);
+    unsigned short GetRiskLevel(UnitInfo *unit);
+
+    void Update(int armor);
+};
+
+#endif /* THREATMAP_HPP */
