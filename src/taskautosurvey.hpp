054d967783b29ccbc3d7052fb1884decb755d8b8
klei1984 <53688147+klei1984@users.noreply.github.com>
2022-08-30 15:46:39 +0200

    Reimplemented the TaskAutoSurvey class.

---
+++ src/taskautosurvey.hpp
@@ -0,0 +1,52 @@
+/* Copyright (c) 2022 M.A.X. Port Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef TASKAUTOSURVEY_HPP
+#define TASKAUTOSURVEY_HPP
+
+#include "task.hpp"
+
+class TaskAutoSurvey : public Task {
+    Point central_site;
+    SmartPointer<UnitInfo> unit;
+    unsigned short radius;
+    unsigned short direction;
+    Point position;
+    Point destination;
+    unsigned short direction2;
+    bool continue_search;
+    bool location_found;
+    unsigned short current_radius;
+
+public:
+    TaskAutoSurvey(UnitInfo* unit);
+    ~TaskAutoSurvey();
+
+    int GetMemoryUse() const;
+    char* WriteStatusLog(char* buffer) const;
+    unsigned char GetType() const;
+    void AddReminder();
+    bool Task_vfunc17(UnitInfo& unit);
+    void RemoveSelf();
+    void Remove(UnitInfo& unit);
+};
+
+#endif /* TASKAUTOSURVEY_HPP */
