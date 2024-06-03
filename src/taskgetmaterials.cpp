klei1984/max

commit 0500d5ceaff65709fb49b7373b05e44672185377

Unit speed is stored as unsigned, but related calculations use signed.

src/taskgetmaterials.cpp
@@ -205,7 +205,7 @@ void TaskGetMaterials::FindTruck() {
          it != UnitsManager_MobileLandSeaUnits.End(); ++it) {
         if ((*it).team == team && UnitsManager_BaseUnits[(*it).unit_type].cargo_type == CARGO_TYPE_RAW &&
             (*it).storage > 0 && (*it).hits > 0 &&
-            ((*it).orders == ORDER_AWAIT || ((*it).orders == ORDER_MOVE && !(*it).speed)) && requestor != *it) {
+            ((*it).orders == ORDER_AWAIT || ((*it).orders == ORDER_MOVE && (*it).speed) == 0) && requestor != *it) {
             bool candidate_found;
 
             if ((*it).GetTask()) {

src/access.cpp
@@ -1048,7 +1048,7 @@ void Access_RenewAttackOrders(SmartList<UnitInfo>& units, uint16_t team) {
             if (((*it).orders == ORDER_MOVE || (*it).orders == ORDER_MOVE_TO_UNIT ||
                  (*it).orders == ORDER_MOVE_TO_ATTACK) &&
                 (*it).state == ORDER_STATE_1) {
-                if ((*it).team == team && (*it).speed != 0 && (*it).engine == 2) {
+                if ((*it).team == team && (*it).speed > 0 && (*it).engine == 2) {
                     UnitsManager_SetNewOrder(&(*it), (*it).orders, ORDER_STATE_INIT);
                 }
             }

src/aiplayer.cpp
@@ -623,7 +623,7 @@ void AiPlayer::DetermineThreats(UnitInfo* unit, Point position, int32_t caution_
         ground_forces_map = air_force_map;
     }
 
-    if (unit->speed <= 0 || teams[unit->team]) {
+    if (!(unit->speed > 0 && !teams[unit->team])) {
         UnitValues* base_values = unit->GetBaseValues();
         int32_t unit_range = base_values->GetAttribute(ATTRIB_RANGE);
         int32_t attack_range = unit_range + base_values->GetAttribute(ATTRIB_ATTACK_RADIUS);

src/paths.cpp
@@ -1459,6 +1459,8 @@ void Paths_TakeStep(UnitInfo* unit, int32_t cost, int32_t param) {
 
     param = 0;
 
+    SDL_assert(unit->speed >= cost);
+
     unit->speed -= cost;
 
     if (unit->group_speed) {
@@ -1526,6 +1528,8 @@ bool Paths_CalculateStep(UnitInfo* unit, int32_t cost, int32_t param, bool is_di
     normalized_cost = (cost + 3) / 4;
 
     if (normalized_cost > unit->speed) {
+        SDL_assert((sizeof(unit->move_fraction) << 8) <= (unit->speed * 4));
+
         unit->move_fraction = unit->speed * 4;
         unit->speed = 0;
 

src/remote.cpp
@@ -81,7 +81,7 @@ struct Packet23Data {
     int16_t grid_y;
     uint8_t team;
     uint8_t hits;
-    int8_t speed;
+    uint8_t speed;
     uint8_t shots;
     int16_t storage;
     uint8_t ammo;

src/taskcreatebuilding.cpp
@@ -132,7 +132,7 @@ void TaskCreateBuilding::MoveToSite() {
 bool TaskCreateBuilding::BuildRoad() {
     bool result;
 
-    if (builder->unit_type == ENGINEER && Task_IsReadyToTakeOrders(&*builder) && builder->speed <= 0 &&
+    if (builder->unit_type == ENGINEER && Task_IsReadyToTakeOrders(&*builder) && builder->speed == 0 &&
         (builder->grid_x != site.x || builder->grid_y != site.y)) {
         if (ini_get_setting(INI_OPPONENT) >= MASTER || builder->storage >= 26) {
             if (GameManager_PlayMode != PLAY_MODE_TURN_BASED || GameManager_ActiveTurnTeam == team) {

src/unitinfo.cpp
@@ -4411,12 +4411,15 @@ int32_t UnitInfo::GetTargetUnitAngle() {
 void UnitInfo::UpdateInfoDisplay() {
     int32_t base_speed = GetBaseValues()->GetAttribute(ATTRIB_SPEED);
     int32_t base_rounds = GetBaseValues()->GetAttribute(ATTRIB_ROUNDS);
+    int32_t unit_speed = speed;
 
-    speed -= (((shots + 1) * base_speed) / base_rounds) - ((shots * base_speed) / base_rounds);
+    unit_speed -= (((shots + 1) * base_speed) / base_rounds) - ((shots * base_speed) / base_rounds);
 
-    if (speed < 0) {
-        speed = 0;
+    if (unit_speed < 0) {
+        unit_speed = 0;
     }
+
+    speed = unit_speed;
 
     if (GameManager_SelectedUnit == this) {
         GameManager_UpdateInfoDisplay(this);

src/unitinfo.hpp
@@ -270,8 +270,8 @@ public:
     uint8_t gold_mining_max;
     uint8_t fuel_mining_max;
     uint8_t hits;
-    int8_t speed;
-    int8_t group_speed;
+    uint8_t speed;
+    uint8_t group_speed;
     uint8_t shots;
     uint8_t move_and_fire;
     int16_t storage;

commit 5e855eb62ffba32c1cb333cf5d779f756295ae5b

Corrected regression from 0500d5c.

src/taskgetmaterials.cpp
@@ -205,7 +205,7 @@ void TaskGetMaterials::FindTruck() {
          it != UnitsManager_MobileLandSeaUnits.End(); ++it) {
         if ((*it).team == team && UnitsManager_BaseUnits[(*it).unit_type].cargo_type == CARGO_TYPE_RAW &&
             (*it).storage > 0 && (*it).hits > 0 &&
-            ((*it).orders == ORDER_AWAIT || ((*it).orders == ORDER_MOVE && (*it).speed) == 0) && requestor != *it) {
+            ((*it).orders == ORDER_AWAIT || ((*it).orders == ORDER_MOVE && (*it).speed == 0)) && requestor != *it) {
             bool candidate_found;
 
             if ((*it).GetTask()) {