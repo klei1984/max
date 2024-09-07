klei1984/max

commit c4f63cbde24ced7dcb6bae6402578ca88ed3d3be

Updated SmartList module
- removed various internal SmartList std::nullptr node checks
- SmartList supports range-based for loops
- added unit tests for SmartList
- prevent comparison of list iterators against std::nullptr
- prevent comparison of list iterators against bool oeprator()
- integrated new implementation of SmartList
- updated SDL2 to v2.28.1
- updated eclipse project configuration
- corrected a reimplementation issue that instructed orders to builders
in corner cases to attempt to remove rubble

src/units_manager.cpp
@@ -3752,7 +3752,7 @@ void UnitsManager_ProcessRemoteOrders() {
         UnitsManager_Unit = nullptr;
     }
 
-    for (SmartList<UnitEvent>::Iterator it = UnitEvent_UnitEvents.Begin(); it; ++it) {
+    for (SmartList<UnitEvent>::Iterator it = UnitEvent_UnitEvents.Begin(); it != UnitEvent_UnitEvents.End(); ++it) {
         (*it).Process();
     }
 
@@ -5665,11 +5665,11 @@ void UnitsManager_BuildClearing(UnitInfo* unit, bool mode) {
         units = &UnitsManager_MobileLandSeaUnits;
     }
 
-    for (SmartList<UnitInfo>::Iterator it = units->Begin(); Access_IsChildOfUnitInList(unit, &it);
+    for (SmartList<UnitInfo>::Iterator it = units->Begin(); Access_IsChildOfUnitInList(unit, units, &it);
          UnitsManager_DestroyUnit(&*it)) {
         if ((*it).unit_type == SEATRANS || (*it).unit_type == AIRTRANS || (*it).unit_type == CLNTRANS) {
             for (SmartList<UnitInfo>::Iterator it2 = UnitsManager_MobileLandSeaUnits.Begin();
-                 Access_IsChildOfUnitInList(&*it, &it2); UnitsManager_DestroyUnit(&*it2)) {
+                 Access_IsChildOfUnitInList(&*it, units, &it2); UnitsManager_DestroyUnit(&*it2)) {
                 ++UnitsManager_TeamInfo[(*it2).team].casualties[(*it2).unit_type];
             }
         }

.cproject
@@ -60,7 +60,7 @@
 				</extensions>
 			</storageModule>
 			<storageModule moduleId="cdtBuildSystem" version="4.0.0">
-				<configuration artifactName="${ProjName}" buildProperties="" description="" id="cdt.managedbuild.toolchain.gnu.base.762831889.1312175591" name="Debug" optionalBuildProperties="org.eclipse.cdt.docker.launcher.containerbuild.property.selectedvolumes=,org.eclipse.cdt.docker.launcher.containerbuild.property.volumes=" parent="org.eclipse.cdt.build.core.emptycfg">
+				<configuration artifactName="${ProjName}" buildProperties="" description="" id="cdt.managedbuild.toolchain.gnu.base.762831889.1312175591" name="Debug" optionalBuildProperties="org.eclipse.cdt.docker.launcher.containerbuild.property.volumes=,org.eclipse.cdt.docker.launcher.containerbuild.property.selectedvolumes=" parent="org.eclipse.cdt.build.core.emptycfg">
 					<folderInfo id="cdt.managedbuild.toolchain.gnu.base.762831889.1312175591." name="/" resourcePath="">
 						<toolChain id="cdt.managedbuild.toolchain.gnu.base.687190123" name="Linux GCC" superClass="cdt.managedbuild.toolchain.gnu.base">
 							<targetPlatform archList="all" binaryParser="" id="cdt.managedbuild.target.gnu.platform.base.655485024" name="Debug Platform" osList="linux,hpux,aix,qnx" superClass="cdt.managedbuild.target.gnu.platform.base"/>
@@ -107,7 +107,7 @@
 				</extensions>
 			</storageModule>
 			<storageModule moduleId="cdtBuildSystem" version="4.0.0">
-				<configuration artifactName="${ProjName}" buildProperties="" description="" id="cdt.managedbuild.toolchain.gnu.base.762831889.1715745439" name="RelWithDebInfo" optionalBuildProperties="org.eclipse.cdt.docker.launcher.containerbuild.property.selectedvolumes=,org.eclipse.cdt.docker.launcher.containerbuild.property.volumes=" parent="org.eclipse.cdt.build.core.emptycfg">
+				<configuration artifactName="${ProjName}" buildProperties="" description="" id="cdt.managedbuild.toolchain.gnu.base.762831889.1715745439" name="RelWithDebInfo" optionalBuildProperties="org.eclipse.cdt.docker.launcher.containerbuild.property.volumes=,org.eclipse.cdt.docker.launcher.containerbuild.property.selectedvolumes=" parent="org.eclipse.cdt.build.core.emptycfg">
 					<folderInfo id="cdt.managedbuild.toolchain.gnu.base.762831889.1715745439." name="/" resourcePath="">
 						<toolChain id="cdt.managedbuild.toolchain.gnu.base.1046201695" name="Linux GCC" superClass="cdt.managedbuild.toolchain.gnu.base">
 							<targetPlatform archList="all" binaryParser="" id="cdt.managedbuild.target.gnu.platform.base.674016309" name="Debug Platform" osList="linux,hpux,aix,qnx" superClass="cdt.managedbuild.target.gnu.platform.base"/>
@@ -210,33 +210,6 @@
 			<path value=""/>
 		</doc-comment-owner>
 	</storageModule>
-	<storageModule moduleId="org.eclipse.cdt.make.core.buildtargets">
-		<buildTargets>
-			<target name="create installers" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
-				<buildCommand>make</buildCommand>
-				<buildArguments/>
-				<buildTarget>package package_source</buildTarget>
-				<stopOnError>true</stopOnError>
-				<useDefaultCommand>false</useDefaultCommand>
-				<runAllBuilders>false</runAllBuilders>
-			</target>
-			<target name="cmake build" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
-				<buildCommand>cmake</buildCommand>
-				<buildArguments>--build .</buildArguments>
-				<stopOnError>true</stopOnError>
-				<useDefaultCommand>false</useDefaultCommand>
-				<runAllBuilders>true</runAllBuilders>
-			</target>
-			<target name="ctest" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
-				<buildCommand>ctest</buildCommand>
-				<buildArguments/>
-				<buildTarget>ctest</buildTarget>
-				<stopOnError>true</stopOnError>
-				<useDefaultCommand>false</useDefaultCommand>
-				<runAllBuilders>false</runAllBuilders>
-			</target>
-		</buildTargets>
-	</storageModule>
 	<storageModule moduleId="scannerConfiguration">
 		<autodiscovery enabled="true" problemReportingEnabled="true" selectedProfileId=""/>
 		<scannerConfigBuildInfo instanceId="cdt.managedbuild.toolchain.gnu.base.762831889;cdt.managedbuild.toolchain.gnu.base.762831889.328966871;cdt.managedbuild.tool.gnu.cpp.compiler.base.1549570889;cdt.managedbuild.tool.gnu.cpp.compiler.input.998778230">
@@ -263,5 +236,40 @@
 		<scannerConfigBuildInfo instanceId="cdt.managedbuild.toolchain.gnu.base.762831889.1019696206;cdt.managedbuild.toolchain.gnu.base.762831889.1019696206.;cdt.managedbuild.tool.gnu.c.compiler.base.1411687919;cdt.managedbuild.tool.gnu.c.compiler.input.2146358315">
 			<autodiscovery enabled="true" problemReportingEnabled="true" selectedProfileId=""/>
 		</scannerConfigBuildInfo>
+	</storageModule>
+	<storageModule moduleId="org.eclipse.cdt.make.core.buildtargets">
+		<buildTargets>
+			<target name="create installers" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
+				<buildCommand>make</buildCommand>
+				<buildArguments/>
+				<buildTarget>package package_source</buildTarget>
+				<stopOnError>true</stopOnError>
+				<useDefaultCommand>false</useDefaultCommand>
+				<runAllBuilders>false</runAllBuilders>
+			</target>
+			<target name="cmake build" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
+				<buildCommand>cmake</buildCommand>
+				<buildArguments>--build .</buildArguments>
+				<stopOnError>true</stopOnError>
+				<useDefaultCommand>false</useDefaultCommand>
+				<runAllBuilders>true</runAllBuilders>
+			</target>
+			<target name="ctest" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
+				<buildCommand>ctest</buildCommand>
+				<buildArguments/>
+				<buildTarget>ctest</buildTarget>
+				<stopOnError>true</stopOnError>
+				<useDefaultCommand>false</useDefaultCommand>
+				<runAllBuilders>false</runAllBuilders>
+			</target>
+			<target name="ctest build" path="" targetID="org.eclipse.cdt.build.MakeTargetBuilder">
+				<buildCommand>make</buildCommand>
+				<buildArguments>-j</buildArguments>
+				<buildTarget>max_tests</buildTarget>
+				<stopOnError>true</stopOnError>
+				<useDefaultCommand>true</useDefaultCommand>
+				<runAllBuilders>true</runAllBuilders>
+			</target>
+		</buildTargets>
 	</storageModule>
 </cproject>

.settings/language.settings.xml
@@ -15,15 +15,12 @@
 	</configuration>
 	<configuration id="cdt.managedbuild.toolchain.gnu.base.762831889.1312175591" name="Debug">
 		<extension point="org.eclipse.cdt.core.LanguageSettingsProvider">
-			<provider copy-of="extension" id="org.eclipse.cdt.ui.UserLanguageSettingsProvider"/>
-			<provider-reference id="org.eclipse.cdt.core.ReferencedProjectsLanguageSettingsProvider" ref="shared-provider"/>
-			<provider class="org.eclipse.cdt.managedbuilder.language.settings.providers.GCCBuildCommandParser" id="org.eclipse.cdt.managedbuilder.core.GCCBuildCommandParser" keep-relative-paths="false" name="CDT GCC Build Output Parser" parameter="([^/\\\\]*)((g?cc)|([gc]\+\+)|(clang))" prefer-non-shared="true"/>
-			<provider class="org.eclipse.cdt.managedbuilder.language.settings.providers.GCCBuiltinSpecsDetector" console="false" env-hash="-714584246141525046" id="org.eclipse.cdt.managedbuilder.core.GCCBuiltinSpecsDetector" keep-relative-paths="false" name="CDT GCC Built-in Compiler Settings" parameter="${COMMAND} ${FLAGS} -E -P -v -dD &quot;${INPUTS}&quot;" prefer-non-shared="true">
+			<provider build-parser-id="org.eclipse.cdt.managedbuilder.core.GCCBuildCommandParser" cdb-modified-time="1689266031282" cdb-path="E:/git/max/Debug/compile_commands.json" class="org.eclipse.cdt.managedbuilder.internal.language.settings.providers.CompilationDatabaseParser" exclude-files="false" id="org.eclipse.cdt.managedbuilder.core.CompilationDatabaseParser" name="Compilation Database Parser" prefer-non-shared="true" store-entries-with-project="true"/>
+			<provider copy-of="extension" id="org.eclipse.cdt.managedbuilder.core.GCCBuildCommandParser"/>
+			<provider class="org.eclipse.cdt.internal.build.crossgcc.CrossGCCBuiltinSpecsDetector" console="false" env-hash="-714584246141525046" id="org.eclipse.cdt.build.crossgcc.CrossGCCBuiltinSpecsDetector" keep-relative-paths="false" name="CDT Cross GCC Built-in Compiler Settings" parameter="${COMMAND} ${FLAGS} -E -P -v -dD &quot;${INPUTS}&quot;" prefer-non-shared="true">
 				<language-scope id="org.eclipse.cdt.core.gcc"/>
 				<language-scope id="org.eclipse.cdt.core.g++"/>
 			</provider>
-			<provider-reference id="org.eclipse.cdt.managedbuilder.core.MBSLanguageSettingsProvider" ref="shared-provider"/>
-			<provider build-parser-id="org.eclipse.cdt.managedbuilder.core.GCCBuildCommandParser" cdb-modified-time="1688723192537" cdb-path="E:/git/max/Debug/compile_commands.json" class="org.eclipse.cdt.managedbuilder.internal.language.settings.providers.CompilationDatabaseParser" exclude-files="true" id="org.eclipse.cdt.managedbuilder.core.CompilationDatabaseParser" name="Compilation Database Parser" prefer-non-shared="true"/>
 		</extension>
 	</configuration>
 	<configuration id="cdt.managedbuild.toolchain.gnu.base.762831889.1715745439" name="RelWithDebInfo">

.settings/org.eclipse.cdt.core.prefs
@@ -29,3 +29,17 @@ environment/project/cdt.managedbuild.toolchain.gnu.base.762831889/PATH/operation
 environment/project/cdt.managedbuild.toolchain.gnu.base.762831889/PATH/value=${path};c\:\\msys64
 environment/project/cdt.managedbuild.toolchain.gnu.base.762831889/append=true
 environment/project/cdt.managedbuild.toolchain.gnu.base.762831889/appendContributed=true
+indexer/indexAllFiles=false
+indexer/indexAllHeaderVersions=true
+indexer/indexAllVersionsSpecificHeaders=
+indexer/indexOnOpen=false
+indexer/indexUnusedHeadersWithAlternateLang=false
+indexer/indexUnusedHeadersWithDefaultLang=false
+indexer/indexerId=org.eclipse.cdt.core.fastIndexer
+indexer/skipFilesLargerThanMB=8
+indexer/skipImplicitReferences=false
+indexer/skipIncludedFilesLargerThanMB=16
+indexer/skipMacroReferences=false
+indexer/skipReferences=false
+indexer/skipTypeReferences=false
+indexer/useHeuristicIncludeResolution=true

cmake/versions.cmake
@@ -10,10 +10,10 @@ set(FREETYPE_URI http://prdownloads.sourceforge.net/freetype/${FREETYPE_FILE})
 set(FREETYPE_HASH ea70022407b00debe025681f974835ad)
 set(FREETYPE_HASH_TYPE MD5)
 
-set(SDL2_VERSION 2.28.0)
+set(SDL2_VERSION 2.28.1)
 set(SDL2_FILE SDL2-${SDL2_VERSION}.tar.gz)
 set(SDL2_URI https://github.com/libsdl-org/SDL/releases/download/release-${SDL2_VERSION}/${SDL2_FILE})
-set(SDL2_HASH f7e53a08a706999ceb033bb911fe3c6e)
+set(SDL2_HASH ccd2cd02b2ad4f71c2588747028953c3)
 set(SDL2_HASH_TYPE MD5)
 
 set(SDL2_MIXER_VERSION 2.6.3)

dependencies/dependencies.md5
@@ -1,6 +1,6 @@
+ccd2cd02b2ad4f71c2588747028953c3 *SDL2-2.28.1.tar.gz
 95b29f0038ec84a611df951d74d99897 *googletest-1.13.0.tar.gz
 ea70022407b00debe025681f974835ad *freetype-2.13.1.tar.gz
-f7e53a08a706999ceb033bb911fe3c6e *SDL2-2.28.0.tar.gz
 d718cd5a59438be666d1575855be72c3 *libiconv-1.17.tar.gz
 ba1cc690ddde85ee90c80e15be9f3bf9 *SDL2_net-2.2.0.tar.gz
 42c50ace7099b43b630d09d871f054c3 *SDL2_mixer-2.6.3.tar.gz

max msys2-i686.launch
@@ -37,8 +37,8 @@
     <listAttribute key="org.eclipse.debug.ui.favoriteGroups">
         <listEntry value="org.eclipse.debug.ui.launchGroup.debug"/>
     </listAttribute>
-    <stringAttribute key="org.eclipse.dsf.launch.MEMORY_BLOCKS" value="&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;no&quot;?&gt;&lt;memoryBlockExpressionList context=&quot;reserved-for-future-use&quot;&gt;&lt;gdbmemoryBlockExpression address=&quot;260280304&quot; label=&quot;data&quot;/&gt;&lt;/memoryBlockExpressionList&gt;"/>
+    <stringAttribute key="org.eclipse.dsf.launch.MEMORY_BLOCKS" value="&lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;no&quot;?&gt;&lt;memoryBlockExpressionList context=&quot;reserved-for-future-use&quot;&gt;&lt;gdbmemoryBlockExpression address=&quot;870518760&quot; label=&quot;0x33e30fe8&quot;/&gt;&lt;/memoryBlockExpressionList&gt;"/>
     <stringAttribute key="process_factory_id" value="org.eclipse.cdt.dsf.gdb.GdbProcessFactory"/>
     <stringAttribute key="provider" value="org.eclipse.linuxtools.profiling.provider.timing.gprof"/>
-    <stringAttribute key="saved_expressions&lt;seperator&gt;Unknown" value="0xca4be30,0xcfc2cb0,0xccc2440,0x209f8f88,0x22a9cf88,0x1f954f88,0x20222fc0,0x2009a880,buf[repeat_count + scan_line_pos],map.map,08D52FFC,0x08D52FFC,0x8D52E80,0xf838ff0,slot-&gt;bitmap.buffer"/>
+    <stringAttribute key="saved_expressions&lt;seperator&gt;Unknown" value="buf[repeat_count + scan_line_pos],map.map,08D52FFC,0x08D52FFC,0x8D52E80,0xf838ff0,slot-&gt;bitmap.buffer,0x1f058fe8,0x5ff878,0x1fc0efe8,0x1f7a4fe8,0x1f7a4ff4,0x3668afe8,0x36688ff0,0x36688ff4"/>
 </launchConfiguration>

commit ea393618565f03731cc81b1fc77a3091bbcbe3b9

Corrected regression from c4f63cbd.

src/units_manager.cpp
@@ -5618,10 +5618,11 @@ void UnitsManager_BuildClearing(UnitInfo* unit, bool mode) {
     }
 
     for (SmartList<UnitInfo>::Iterator it = units->Begin(); Access_IsChildOfUnitInList(unit, units, &it);
-         UnitsManager_DestroyUnit(&*it)) {
+         UnitsManager_DestroyUnit(it->Get())) {
         if ((*it).GetUnitType() == SEATRANS || (*it).GetUnitType() == AIRTRANS || (*it).GetUnitType() == CLNTRANS) {
             for (SmartList<UnitInfo>::Iterator it2 = UnitsManager_MobileLandSeaUnits.Begin();
-                 Access_IsChildOfUnitInList(&*it, units, &it2); UnitsManager_DestroyUnit(&*it2)) {
+                 Access_IsChildOfUnitInList(it->Get(), &UnitsManager_MobileLandSeaUnits, &it2);
+                 UnitsManager_DestroyUnit(it2->Get())) {
                 ++UnitsManager_TeamInfo[(*it2).team].casualties[(*it2).GetUnitType()];
             }
         }