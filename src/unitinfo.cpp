klei1984/max

commit 7664e4b923fd1a1927979f9c553796ccb7d63948

Fixed defects 100 and 234.

src/unitinfo.cpp
@@ -523,7 +523,7 @@ UnitInfo::UnitInfo()
       ammo(0),
       targeting_mode(0),
       enter_mode(0),
-      cursor(0),
+      cursor(CURSOR_HIDDEN),
       recoil_delay(0),
       delayed_reaction(0),
       damaged_this_turn(false),
@@ -557,13 +557,40 @@ UnitInfo::UnitInfo()
       field_221(0) {}
 
 UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t angle)
-    : orders(ORDER_AWAIT),
+    : name(nullptr),
+      sound(SFX_TYPE_INVALID),
+      unit_type(unit_type),
+      popup(nullptr),
+      sound_table(nullptr),
+      flags(UnitsManager_BaseUnits[unit_type].flags | UnitsManager_TeamInfo[team].team_units->hash_team_id),
+      x(-1),
+      y(-1),
+      grid_x(-1),
+      grid_y(-1),
+      color_cycling_lut(UnitsManager_TeamInfo[team].team_units->color_index_table),
+      team(team),
+      unit_id(0),
+      brightness(UINT8_MAX),
+      angle(angle),
+      max_velocity(0),
+      velocity(0),
+      scaler_adjust(0),
+      turret_angle(0),
+      turret_offset_x(0),
+      turret_offset_y(0),
+      total_images(0),
+      image_base(0),
+      turret_image_base(0),
+      firing_image_base(0),
+      connector_image_base(0),
+      image_index_max(0),
+      orders(ORDER_AWAIT),
       state(ORDER_STATE_EXECUTING_ORDER),
       prior_orders(ORDER_AWAIT),
       prior_state(ORDER_STATE_EXECUTING_ORDER),
-      laying_state(0),
       target_grid_x(0),
       target_grid_y(0),
+      build_time(0),
       total_mining(0),
       raw_mining(0),
       fuel_mining(0),
@@ -571,40 +598,48 @@ UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t ang
       raw_mining_max(0),
       gold_mining_max(0),
       fuel_mining_max(0),
-      popup(nullptr),
-      sound_table(nullptr),
-      in_transit(false),
-      pin_count(0),
-      name(nullptr),
-      unit_type(unit_type),
-      team(team),
-      id(id),
-      disabled_reaction_fire(false),
+      hits(0),
+      speed(0),
+      group_speed(0),
+      shots(0),
+      move_and_fire(0),
+      storage(0),
+      ammo(0),
+      targeting_mode(0),
+      enter_mode(0),
       cursor(CURSOR_HIDDEN),
-      base_values(UnitsManager_GetCurrentUnitValues(&UnitsManager_TeamInfo[team], unit_type)),
-      unit_id(0),
-      build_rate(1),
-      field_221(0),
-      energized(false),
-      build_time(0),
-      flags(UnitsManager_BaseUnits[unit_type].flags | UnitsManager_TeamInfo[team].team_units->hash_team_id),
-      color_cycling_lut(UnitsManager_TeamInfo[team].team_units->color_index_table),
-      brightness(0xFF),
+      recoil_delay(0),
       delayed_reaction(0),
       damaged_this_turn(false),
-      repeat_build(0),
-      field_165(1),
+      disabled_reaction_fire(false),
+      auto_survey(false),
+      research_topic(false),
+      moved(0),
+      bobbed(false),
+      engine(0),
+      weapon(0),
+      comm(0),
+      fuel_distance(0),
       move_fraction(0),
       connectors(0),
-      scaler_adjust(0),
-      research_topic(0),
-      velocity(0),
-      sound(SFX_TYPE_INVALID),
+      shake_effect_state(0),
+      base_values(UnitsManager_GetCurrentUnitValues(&UnitsManager_TeamInfo[team], unit_type)),
+      build_rate(0),
+      repeat_build(0),
+      energized(false),
+      id(id),
       unit_list(nullptr),
-      group_speed(0),
-      shadow_offset(0, 0),
-      auto_survey(false),
-      angle(angle) {
+      in_transit(false),
+      pin_count(0),
+      field_165(true),
+      laying_state(0),
+      visible_to_team(),
+      spotted_by_team(),
+      sprite_bounds(),
+      shadow_bounds(),
+      image_index(0),
+      turret_image_index(0),
+      field_221(0) {
     BaseUnit* unit;
 
     rect_init(&sprite_bounds, 0, 0, 0, 0);
@@ -3009,6 +3044,12 @@ void UnitInfo::FileLoad(SmartFileReader& file) noexcept {
     file.Read(enter_mode);
     file.Read(cursor);
     file.Read(recoil_delay);
+
+    /* correct negative values */
+    if (recoil_delay < 0) {
+        recoil_delay = 0;
+    }
+
     file.Read(delayed_reaction);
     file.Read(damaged_this_turn);
     file.Read(research_topic);

src/access.cpp
@@ -1965,7 +1965,8 @@ void Access_MultiSelect(UnitInfo* unit, Rect* bounds) {
             (unit->flags & (MOBILE_AIR_UNIT | MOBILE_SEA_UNIT | MOBILE_LAND_UNIT)) &&
             unit->GetOrderState() == ORDER_STATE_EXECUTING_ORDER && unit->grid_x >= selection.ulx &&
             unit->grid_x <= selection.lrx && unit->grid_y >= selection.uly && unit->grid_y <= selection.lry &&
-            unit->recoil_delay <= 0) {
+            !(unit->GetOrder() == ORDER_DISABLE ||
+              (unit->GetOrder() == ORDER_IDLE && unit->GetPriorOrder() == ORDER_DISABLE))) {
             parent = unit;
             unit->ClearUnitList();
             unit->AllocateUnitList();
@@ -1989,7 +1990,8 @@ void Access_MultiSelect(UnitInfo* unit, Rect* bounds) {
             if (unit2 &&
                 (unit2->GetOrderState() == ORDER_STATE_EXECUTING_ORDER ||
                  unit2->GetOrderState() == ORDER_STATE_READY_TO_EXECUTE_ORDER) &&
-                unit2->recoil_delay <= 0) {
+                !(unit2->GetOrder() == ORDER_DISABLE ||
+                  (unit2->GetOrder() == ORDER_IDLE && unit2->GetPriorOrder() == ORDER_DISABLE))) {
                 unit2->ClearUnitList();
 
                 if (!parent) {

src/game_manager.cpp
@@ -6411,7 +6411,12 @@ bool GameManager_UpdateSelection(UnitInfo* unit1, UnitInfo* unit2, int32_t grid_
                 GameManager_PlayMode == PLAY_MODE_SIMULTANEOUS_MOVES) &&
                UnitsManager_TeamInfo[GameManager_ActiveTurnTeam].team_type == TEAM_TYPE_PLAYER) {
         if (GameManager_IsShiftKeyPressed && unit1 && unit2) {
-            if (unit2->team == GameManager_PlayerTeam) {
+            if (unit2->team == GameManager_PlayerTeam &&
+                !(unit2->GetOrder() == ORDER_DISABLE ||
+                  (unit2->GetOrder() == ORDER_IDLE && unit2->GetPriorOrder() == ORDER_DISABLE)) &&
+                unit1->team == GameManager_PlayerTeam &&
+                !(unit1->GetOrder() == ORDER_DISABLE ||
+                  (unit1->GetOrder() == ORDER_IDLE && unit1->GetPriorOrder() == ORDER_DISABLE))) {
                 if (unit2->GetFirstFromUnitList() == unit1) {
                     unit2->ClearUnitList();
 

commit cdda90634b79378e1aa1b355558f9b9c72b74cab

Fixed regression from 7664e4b.

src/unitinfo.cpp
@@ -624,7 +624,7 @@ UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t ang
       connectors(0),
       shake_effect_state(0),
       base_values(UnitsManager_GetCurrentUnitValues(&UnitsManager_TeamInfo[team], unit_type)),
-      build_rate(0),
+      build_rate(1),
       repeat_build(0),
       energized(false),
       id(id),