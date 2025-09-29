klei1984/max

commit 29b75c76a0d2593e2df581dbda0da4b20c467a1a

Fixed defect 32.

src/game_manager.cpp
@@ -4541,15 +4541,16 @@ uint8_t GameManager_GetWindowCursor(int32_t grid_x, int32_t grid_y) {
     }
 
     if (GameManager_SelectedUnit->GetOrderState() == ORDER_STATE_EXECUTING_ORDER) {
-        UnitInfo* unit;
+        UnitInfo* enemy;
 
-        unit = Access_GetEnemyUnit(GameManager_PlayerTeam, grid_x, grid_y, SELECTABLE);
+        enemy = Access_GetEnemyUnit(GameManager_PlayerTeam, grid_x, grid_y, SELECTABLE);
 
-        if (unit && unit->GetUnitType() != CNCT_4W &&
-            (!(unit->flags & GROUND_COVER) || unit->GetUnitType() == LANDMINE || unit->GetUnitType() == SEAMINE)) {
+        if (enemy && enemy->GetUnitType() != CNCT_4W &&
+            (!(enemy->flags & GROUND_COVER) || enemy->GetUnitType() == LANDMINE || enemy->GetUnitType() == SEAMINE) &&
+            Access_IsValidAttackTarget(GameManager_SelectedUnit.Get(), enemy)) {
             uint8_t result;
 
-            result = GameManager_GetMilitaryCursor(unit, grid_x, grid_y);
+            result = GameManager_GetMilitaryCursor(enemy, grid_x, grid_y);
 
             if (result != CURSOR_UNIT_NO_GO || !team_unit) {
                 return result;

commit 8673220f97e3f01ba6c8e31fbd4d4f98d15fb8a4

Fixed regression from 29b75c7.

src/game_manager.cpp
@@ -6083,8 +6083,10 @@ UnitInfo* GameManager_GetUnitWithCargoType(Complex* complex, int32_t cargo_type)
 int32_t GameManager_GetUnitActionCursor(UnitInfo* unit1, int32_t grid_x, int32_t grid_y, UnitInfo* unit2) {
     int32_t result;
 
+    uint32_t flags = GameManager_MaxSpy ? AccessModifier_IgnoreVisibility : AccessModifier_NoModifiers;
+
     if (Access_IsAccessible(unit1->GetUnitType(), GameManager_PlayerTeam, grid_x, grid_y,
-                            AccessModifier_SameClassBlocks)) {
+                            AccessModifier_SameClassBlocks | flags)) {
         if (unit2 && (unit2->flags & MOBILE_AIR_UNIT)) {
             result = CURSOR_FRIEND;
 