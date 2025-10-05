klei1984/max

commit c1387c70f6cb140f86ca9f33716110ea7a9d76d1

Code refactorings due to integer promotion rules for 64 bit types

src/paths.cpp
@@ -501,7 +501,7 @@ Point GroundPath::GetPosition(UnitInfo* unit) const {
 
         move_fraction = unit->move_fraction;
 
-        for (int32_t i = index; i < steps->GetCount(); ++i) {
+        for (uint32_t i = index; i < steps->GetCount(); ++i) {
             step = steps[i];
 
             point.x = position.x + step->x;
@@ -540,7 +540,7 @@ Point GroundPath::GetPosition(UnitInfo* unit) const {
 bool GroundPath::IsInPath(int32_t grid_x, int32_t grid_y) const {
     Point point(0, 0);
 
-    for (int32_t i = index; i < steps.GetCount(); ++i) {
+    for (uint32_t i = index; i < steps.GetCount(); ++i) {
         if (point.x == grid_x && point.y == grid_y) {
             return true;
         }
@@ -580,7 +580,7 @@ int32_t GroundPath::GetMovementCost(UnitInfo* unit) {
 
     result = 0;
 
-    for (int32_t i = index; i < steps.GetCount(); ++i) {
+    for (uint32_t i = index; i < steps.GetCount(); ++i) {
         PathStep* step = steps[i];
 
         if (step->x || step->y) {
@@ -828,7 +828,7 @@ void GroundPath::Draw(UnitInfo* unit, WindowInfo* window) {
     int32_t scaled_grid_y;
     int32_t pixel_grid_x;
     int32_t pixel_grid_y;
-    int32_t steps_count;
+    uint32_t steps_count;
     int32_t path_x = 0;
     int32_t path_y = 0;
     int32_t cost = 0;
@@ -856,7 +856,7 @@ void GroundPath::Draw(UnitInfo* unit, WindowInfo* window) {
 
     steps_count = steps->GetCount();
 
-    for (int32_t i = index; i <= steps_count; ++i) {
+    for (uint32_t i = index; i <= steps_count; ++i) {
         if (i != steps_count) {
             path_x = steps[i]->x;
             path_y = steps[i]->y;
@@ -1632,7 +1632,7 @@ bool Paths_IsOccupied(int32_t grid_x, int32_t grid_y, int32_t angle, int32_t tea
 void Paths_ReserveSite(const Point site) noexcept { Paths_SiteReservations.PushBack(&site); }
 
 void Paths_RemoveSiteReservation(const Point site) noexcept {
-    const int32_t position{Paths_SiteReservations->Find(&site)};
+    const int64_t position{Paths_SiteReservations->Find(&site)};
 
     SDL_assert(position != -1);
 

src/paths.hpp
@@ -68,7 +68,7 @@ public:
 };
 
 class GroundPath : public UnitPath {
-    uint16_t index;
+    uint32_t index;
     SmartObjectArray<PathStep> steps;
 
 public:

src/unitinfo.cpp
@@ -3203,13 +3203,13 @@ void UnitInfo::WritePacket(NetPacket& packet) {
 
     packet << build_list.GetCount();
 
-    for (int32_t i = 0; i < build_list.GetCount(); ++i) {
+    for (uint32_t i = 0; i < build_list.GetCount(); ++i) {
         packet << *build_list[i];
     }
 }
 
 void UnitInfo::ReadPacket(NetPacket& packet) {
-    uint16_t unit_count;
+    uint32_t unit_count;
     ResourceID list_item;
 
     packet >> team;
@@ -3224,7 +3224,7 @@ void UnitInfo::ReadPacket(NetPacket& packet) {
 
     packet >> unit_count;
 
-    for (int32_t i = 0; i < unit_count; ++i) {
+    for (uint32_t i = 0; i < unit_count; ++i) {
         packet >> list_item;
 
         build_list.PushBack(&list_item);

src/abstractupgrademenu.cpp
@@ -375,7 +375,7 @@ void AbstractUpgradeMenu::DrawUnitInfo(ResourceID unit_type) {
     }
 
     upgrade_control_count = 0;
-    upgrade_control_next_uly = cost_background->GetULY();
+    upgrade_control_next_uly = static_cast<uint16_t>(cost_background->GetULY());
     button_background->Write(&window1);
 
     if (unit_type != INVALID_ID && Builder_IsBuildable(unit_type)) {
@@ -423,10 +423,12 @@ void AbstractUpgradeMenu::PopulateTeamUnitsList() {
         }
     }
 
-    for (int32_t i = 0; i < unit_types.GetCount() - 1; ++i) {
-        for (int32_t j = i + 1; unit_types.GetCount() > j; ++j) {
-            if (IsBetterUnit(*unit_types[i], *unit_types[j])) {
-                std::swap(*unit_types[i], *unit_types[j]);
+    if (unit_types.GetCount() > 1) {
+        for (uint32_t i = 0; i < static_cast<uint32_t>(static_cast<int64_t>(unit_types.GetCount()) - 1); ++i) {
+            for (uint32_t j = i + 1; unit_types.GetCount() > j; ++j) {
+                if (IsBetterUnit(*unit_types[i], *unit_types[j])) {
+                    std::swap(*unit_types[i], *unit_types[j]);
+                }
             }
         }
     }
@@ -508,7 +510,7 @@ void AbstractUpgradeMenu::DrawUnitStats(ResourceID unit_type) {
 }
 
 void AbstractUpgradeMenu::AbstractUpgradeMenu_vfunc7() {
-    for (int32_t i = 0; i < UNIT_END; ++i) {
+    for (uint16_t i = 0; i < UNIT_END; ++i) {
         if (*unitvalues_actual[i] != *unitvalues_base[i]) {
             unitvalues_actual[i]->UpdateVersion();
             unitvalues_actual[i]->SetUnitsBuilt(0);

src/abstractupgrademenu.hpp
@@ -33,8 +33,8 @@ protected:
     WindowInfo window1;
     WindowInfo window2;
     uint16_t team;
-    uint16_t start_gold;
-    uint16_t team_gold;
+    int32_t start_gold;
+    int32_t team_gold;
     uint16_t upgrade_control_count;
     uint16_t upgrade_control_next_uly;
     ResourceID interface_icon_full;

src/access.cpp
@@ -251,8 +251,8 @@ uint32_t Access_IsAccessible(ResourceID unit_type, uint16_t team, int32_t grid_x
 bool Access_FindReachableSpotInt(ResourceID unit_type, UnitInfo* unit, int16_t* grid_x, int16_t* grid_y,
                                  int32_t range_limit, int32_t mode, int32_t direction) {
     UnitValues* unit_values;
-    int32_t offset_x{0};
-    int32_t offset_y{0};
+    int16_t offset_x{0};
+    int16_t offset_y{0};
 
     unit_values = unit->GetBaseValues();
 
@@ -323,7 +323,7 @@ void Access_InitStealthMaps() {
     Access_InitUnitStealthStatus(UnitsManager_ParticleUnits);
     Access_InitUnitStealthStatus(UnitsManager_StationaryUnits);
 
-    for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+    for (uint16_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
         if (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE) {
             memset(UnitsManager_TeamInfo[team].heat_map_complete, 0, map_cell_count);
             memset(UnitsManager_TeamInfo[team].heat_map_stealth_sea, 0, map_cell_count);
@@ -639,14 +639,14 @@ void Access_DrawUnit(UnitInfo* unit) {
     const int32_t map_offset{ResourceManager_MapSize.x * unit->grid_y + unit->grid_x};
 
     if (GameManager_AllVisible) {
-        for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+        for (uint16_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
             if (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE) {
                 unit->SpotByTeam(team);
             }
         }
     }
 
-    for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+    for (uint16_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
         if (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE) {
             if (UnitsManager_TeamInfo[team].heat_map_stealth_sea[map_offset] && UnitsManager_IsUnitUnderWater(unit)) {
                 unit->SpotByTeam(team);
@@ -671,7 +671,7 @@ uint32_t Access_GetTargetClass(UnitInfo* unit) {
     uint32_t result;
 
     if (unit->GetUnitType() == COMMANDO || UnitsManager_IsUnitUnderWater(unit)) {
-        for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+        for (uint16_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
             if (unit->team != team && UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE &&
                 unit->IsVisibleToTeam(team)) {
                 if (unit->GetUnitType() == COMMANDO) {
@@ -714,7 +714,7 @@ void Access_UpdateMapStatus(UnitInfo* unit, bool mode) {
         }
 
         if (GameManager_AllVisible) {
-            for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+            for (uint16_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
                 if (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE) {
                     unit->SpotByTeam(team);
                 }
@@ -731,7 +731,7 @@ void Access_UpdateMapStatus(UnitInfo* unit, bool mode) {
             }
 
             if ((unit->flags & SELECTABLE) && UnitsManager_TeamInfo[unit->team].team_type != TEAM_TYPE_NONE) {
-                uint8_t enemy_target_class = TARGET_CLASS_NONE;
+                uint32_t enemy_target_class = TARGET_CLASS_NONE;
                 int32_t scan = unit->GetBaseValues()->GetAttribute(ATTRIB_SCAN);
                 Rect zone;
 
@@ -983,7 +983,7 @@ int32_t Access_FindUnitInUnitList(UnitInfo* unit) {
 
     for (uint32_t i = 0; i < std::size(Access_UnitsLists); ++i) {
         if (Access_UnitsLists[i]->Find(*unit) != Access_UnitsLists[i]->End()) {
-            result = i;
+            result = static_cast<int32_t>(i);
             break;
         }
     }
@@ -1074,12 +1074,10 @@ void Access_RenewAttackOrders(SmartList<UnitInfo>& units, uint16_t team) {
 }
 
 bool Access_UpdateGroupSpeed(UnitInfo* unit) {
-    int32_t max_speed = INT32_MAX;
-    SmartList<UnitInfo>* units;
+    int32_t max_speed = static_cast<int32_t>(INT32_MAX) - 1L;
+    auto units = unit->GetUnitList();
 
-    units = unit->GetUnitList();
-
-    for (SmartList<UnitInfo>::Iterator it = units->Begin(); it != units->End(); ++it) {
+    for (auto it = units->Begin(); it != units->End(); ++it) {
         if (((*it).GetOrder() == ORDER_MOVE || (*it).GetOrder() == ORDER_MOVE_TO_ATTACK) &&
             ((*it).GetOrderState() == ORDER_STATE_IN_PROGRESS || (*it).GetOrderState() == ORDER_STATE_IN_TRANSITION ||
              (*it).GetOrderState() == ORDER_STATE_NEW_ORDER)) {
@@ -1091,7 +1089,7 @@ bool Access_UpdateGroupSpeed(UnitInfo* unit) {
         }
     }
 
-    for (SmartList<UnitInfo>::Iterator it = units->Begin(); it != units->End(); ++it) {
+    for (auto it = units->Begin(); it != units->End(); ++it) {
         (*it).group_speed = max_speed + 1;
     }
 

src/accessmap.cpp
@@ -24,19 +24,20 @@
 #include "resource_manager.hpp"
 
 AccessMap::AccessMap() {
-    size = ResourceManager_MapSize.x;
+    size_x = static_cast<uint32_t>(ResourceManager_MapSize.x);
+    uint32_t size_y = static_cast<uint32_t>(ResourceManager_MapSize.y);
 
-    map = new (std::nothrow) uint8_t*[ResourceManager_MapSize.x];
+    map = new (std::nothrow) uint8_t*[size_x];
 
-    for (int32_t i = 0; i < ResourceManager_MapSize.x; ++i) {
-        map[i] = new (std::nothrow) uint8_t[ResourceManager_MapSize.y];
+    for (uint32_t i = 0; i < size_x; ++i) {
+        map[i] = new (std::nothrow) uint8_t[size_y];
 
-        memset(map[i], 0, ResourceManager_MapSize.y);
+        SDL_memset(map[i], 0, size_y);
     }
 }
 
 AccessMap::~AccessMap() {
-    for (int32_t i = 0; i < size; ++i) {
+    for (uint32_t i = 0; i < size_x; ++i) {
         delete[] map[i];
     }
 
@@ -45,4 +46,8 @@ AccessMap::~AccessMap() {
 
 uint8_t** AccessMap::GetMap() const { return map; }
 
-uint8_t* AccessMap::GetMapColumn(int32_t index) const { return map[index]; }
+uint8_t* AccessMap::GetMapColumn(const uint32_t index) const {
+    SDL_assert(index < size_x);
+
+    return (index < size_x) ? map[index] : nullptr;
+}

src/accessmap.hpp
@@ -26,14 +26,14 @@
 
 class AccessMap {
     uint8_t **map;
-    int16_t size;
+    uint32_t size_x;
 
 public:
     AccessMap();
     ~AccessMap();
 
     uint8_t **GetMap() const;
-    uint8_t *GetMapColumn(int32_t index) const;
+    uint8_t *GetMapColumn(const uint32_t index) const;
 };
 
 #endif /* ACCESSMAP_HPP */

commit 1386645b12bffd55eda60c85517e547cfa161cf0

Fixed regression from c1387c7.

src/paths.cpp
@@ -460,7 +460,17 @@ void GroundPath::FileLoad(SmartFileReader& file) noexcept {
 
     file.Read(x_end);
     file.Read(y_end);
-    file.Read(index);
+
+    if (file.GetFormat() == SmartFileFormat::V70) {
+        uint16_t local_index;
+
+        file.Read(local_index);
+
+        index = local_index;
+
+    } else {
+        file.Read(index);
+    }
 
     count = file.ReadObjectCount();
 
@@ -1014,7 +1024,7 @@ void GroundPath::AddStep(int32_t step_x, int32_t step_y) {
 
 SmartObjectArray<PathStep> GroundPath::GetSteps() { return steps; }
 
-uint16_t GroundPath::GetPathStepIndex() const { return index; }
+uint32_t GroundPath::GetPathStepIndex() const { return index; }
 
 bool Paths_RequestPath(UnitInfo* unit, int32_t mode) {
     bool result;

src/paths.hpp
@@ -96,7 +96,7 @@ public:
 
     void AddStep(int32_t step_x, int32_t step_y);
     SmartObjectArray<PathStep> GetSteps();
-    uint16_t GetPathStepIndex() const;
+    uint32_t GetPathStepIndex() const;
 };
 
 class AirPath : public UnitPath {

src/unitinfo.cpp
@@ -2459,7 +2459,7 @@ void UnitInfo::Move() {
             if (path) {
                 GroundPath* ground_path = dynamic_cast<GroundPath*>(&*path);
                 SmartObjectArray<PathStep> path_steps = ground_path->GetSteps();
-                int32_t path_step_index = ground_path->GetPathStepIndex();
+                uint32_t path_step_index = ground_path->GetPathStepIndex();
 
                 path_steps[path_step_index]->x -= offset_x;
                 path_steps[path_step_index]->y -= offset_y;