klei1984/max

commit ea5ac8556c79eff50c526f86cf70996b56eab8ef

Removed ticks timer feature from GNW.
Change log:
- Replaced configurable GNW timer module API with a new API that is
always milliseconds based. The new API does not have a configuration
interface. The backend is SDL2 32 bit timer interface that is replaced
by the 64 bit timer interface in SDL3.
- Removed GNW Window manager option to run directly from system ticks
timer instead of milliseconds timer.
- Reintegrated the new GNW timer module API into the game.

Resolved reimplementation issues:
- The old PIT timer simulation based GNW timer interface only worked for
about 1 hour of game play.
- The turn timer did not update in DialogMenu class based menus.

Risks:
- The TIMER_FPS_TO_MS() macro is not as accurate as TIMER_FPS_TO_TICKS()
was.

src/taskclearzone.cpp
@@ -162,7 +162,7 @@ void TaskClearZone::EndTurn() {
 
                 if (state == CLEARZONE_STATE_WAITING) {
                     while (!ExamineZones()) {
-                        if (timer_get_stamp32() - Paths_LastTimeStamp > Paths_TimeLimit) {
+                        if (Paths_HaveTimeToThink()) {
                             state = CLEARZONE_STATE_WAITING;
 
                             RemindTurnEnd(true);
@@ -432,8 +432,7 @@ void TaskClearZone::SearchMap() {
 
     state = CLEARZONE_STATE_SEARCHING_MAP;
 
-    while (zone_squares.GetCount() > 0 && state == CLEARZONE_STATE_SEARCHING_MAP &&
-           timer_get_stamp32() - Paths_LastTimeStamp < Paths_TimeLimit) {
+    while (zone_squares.GetCount() > 0 && state == CLEARZONE_STATE_SEARCHING_MAP && Paths_HaveTimeToThink()) {
         ZoneSquare zone_square(*zone_squares[0]);
 
         zone_squares.Remove(0);

src/ailog.cpp
@@ -32,7 +32,7 @@ int AiLog::AiLog_EntryCount;
 
 AiLog::AiLog(const char* format, ...) {
     if (AiLog_File.is_open()) {
-        time_stamp = timer_get_stamp32();
+        time_stamp = timer_get();
 
         va_list args;
 
@@ -48,9 +48,9 @@ AiLog::~AiLog() {
     --AiLog_SectionCount;
 
     if (AiLog_File.is_open()) {
-        unsigned int elapsed_time = timer_elapsed_time_ms(time_stamp);
+        unsigned int elapsed_time = timer_elapsed_time(time_stamp);
 
-        if (elapsed_time >= 20) {
+        if (elapsed_time >= TIMER_FPS_TO_MS(50)) {
             Log("log section complete, %li msecs elapsed", elapsed_time);
 
         } else {

src/dialogmenu.cpp
@@ -88,12 +88,12 @@ bool DialogMenu::ProcessKey(int key) {
                 }
 
                 do {
-                    unsigned int time_Stamp = timer_get_stamp32();
+                    unsigned int time_Stamp = timer_get();
 
                     --row_offset;
                     DrawText();
 
-                    while (timer_get_stamp32() - time_Stamp < TIMER_FPS_TO_TICKS(96)) {
+                    while (timer_get() - time_Stamp < TIMER_FPS_TO_MS(96)) {
                     }
                 } while (num_rows != row_offset);
             }
@@ -112,12 +112,12 @@ bool DialogMenu::ProcessKey(int key) {
                 num_rows = row_offset + max_row_count;
 
                 do {
-                    unsigned int time_Stamp = timer_get_stamp32();
+                    unsigned int time_Stamp = timer_get();
 
                     ++row_offset;
                     DrawText();
 
-                    while (timer_get_stamp32() - time_Stamp < TIMER_FPS_TO_TICKS(96)) {
+                    while (timer_get() - time_Stamp < TIMER_FPS_TO_MS(96)) {
                     }
                 } while (num_rows != row_offset);
             }

src/game_manager.cpp
@@ -533,12 +533,12 @@ struct MenuFlic {
 };
 
 static struct ColorCycleData GameManager_ColorCycleTable[] = {
-    COLOR_CYCLE_DATA(9, 12, 0, TIMER_FPS_TO_TICKS(9), 0),     COLOR_CYCLE_DATA(13, 16, 1, TIMER_FPS_TO_TICKS(6), 0),
-    COLOR_CYCLE_DATA(17, 20, 1, TIMER_FPS_TO_TICKS(9), 0),    COLOR_CYCLE_DATA(21, 24, 1, TIMER_FPS_TO_TICKS(6), 0),
-    COLOR_CYCLE_DATA(25, 30, 1, TIMER_FPS_TO_TICKS(2), 0),    COLOR_CYCLE_DATA(31, 31, 1, TIMER_FPS_TO_TICKS(6), 0),
-    COLOR_CYCLE_DATA(96, 102, 1, TIMER_FPS_TO_TICKS(8), 0),   COLOR_CYCLE_DATA(103, 109, 1, TIMER_FPS_TO_TICKS(8), 0),
-    COLOR_CYCLE_DATA(110, 116, 1, TIMER_FPS_TO_TICKS(10), 0), COLOR_CYCLE_DATA(117, 122, 1, TIMER_FPS_TO_TICKS(6), 0),
-    COLOR_CYCLE_DATA(123, 127, 1, TIMER_FPS_TO_TICKS(6), 0),
+    COLOR_CYCLE_DATA(9, 12, 0, TIMER_FPS_TO_MS(9), 0),     COLOR_CYCLE_DATA(13, 16, 1, TIMER_FPS_TO_MS(6), 0),
+    COLOR_CYCLE_DATA(17, 20, 1, TIMER_FPS_TO_MS(9), 0),    COLOR_CYCLE_DATA(21, 24, 1, TIMER_FPS_TO_MS(6), 0),
+    COLOR_CYCLE_DATA(25, 30, 1, TIMER_FPS_TO_MS(2), 0),    COLOR_CYCLE_DATA(31, 31, 1, TIMER_FPS_TO_MS(6), 0),
+    COLOR_CYCLE_DATA(96, 102, 1, TIMER_FPS_TO_MS(8), 0),   COLOR_CYCLE_DATA(103, 109, 1, TIMER_FPS_TO_MS(8), 0),
+    COLOR_CYCLE_DATA(110, 116, 1, TIMER_FPS_TO_MS(10), 0), COLOR_CYCLE_DATA(117, 122, 1, TIMER_FPS_TO_MS(6), 0),
+    COLOR_CYCLE_DATA(123, 127, 1, TIMER_FPS_TO_MS(6), 0),
 };
 
 static struct MenuGuiItem GameManager_MenuItems[] = {
@@ -2028,12 +2028,12 @@ void GameManager_GameSetup(int game_state) {
         }
 
         while (zoom_level <= max_zoom_level) {
-            timestamp = timer_get_stamp32();
+            timestamp = timer_get();
 
             GameManager_UpdateMainMapView(0, zoom_level, 0, false);
             GameManager_ProcessTick(false);
 
-            while ((timer_get_stamp32() - timestamp) < TIMER_FPS_TO_TICKS(48)) {
+            while ((timer_get() - timestamp) < TIMER_FPS_TO_MS(48)) {
             }
 
             zoom_level *= 2;
@@ -2329,7 +2329,7 @@ void GameManager_UpdateTurnTimer(bool mode, int turn_time) {
         Remote_UpdatePauseTimer = mode;
 
         if (Remote_UpdatePauseTimer) {
-            Remote_PauseTimeStamp = timer_get_stamp32();
+            Remote_PauseTimeStamp = timer_get();
         }
     } else {
         Remote_UpdatePauseTimer = false;
@@ -3331,7 +3331,7 @@ void GameManager_MenuAnimateDisplayControls() {
     bottom_window = WindowManager_GetWindow(WINDOW_BOTTOM_INSTRUMENTS_WINDOW);
 
     for (int i = 0; i < 5; ++i) {
-        time_stamp = timer_get_stamp32();
+        time_stamp = timer_get();
         WindowManager_LoadSimpleImage(static_cast<ResourceID>(PNLSEQ_1 + i), top_window->window.ulx,
                                       top_window->window.uly, true);
         win_draw_rect(top_window->id, &top_window->window);
@@ -3344,7 +3344,7 @@ void GameManager_MenuAnimateDisplayControls() {
 
         process_bk();
 
-        while ((timer_get_stamp32() - time_stamp) < TIMER_FPS_TO_TICKS(24)) {
+        while ((timer_get() - time_stamp) < TIMER_FPS_TO_MS(24)) {
         }
     }
 }
@@ -3457,12 +3457,12 @@ void GameManager_NotifyEvent(UnitInfo* unit, int event) {
             sprintf(text, GameManager_EventStrings_EnemySpotted[UnitsManager_BaseUnits[unit->unit_type].gender],
                     UnitsManager_BaseUnits[unit->unit_type].singular_name);
 
-            if (!GameManager_IsAtGridPosition(unit) && timer_elapsed_time_ms(GameManager_NotifyTimeout) > 5000) {
+            if (!GameManager_IsAtGridPosition(unit) && timer_elapsed_time(GameManager_NotifyTimeout) > 5000) {
                 resource_id1 = V_M070;
                 resource_id2 = V_F071;
             }
 
-            GameManager_NotifyTimeout = timer_get_stamp32();
+            GameManager_NotifyTimeout = timer_get();
 
         } break;
 
@@ -5044,9 +5044,9 @@ bool GameManager_SyncTurnTimer() {
         unsigned int time_stamp;
         unsigned int seconds_elapsed;
 
-        time_stamp = timer_get_stamp32();
+        time_stamp = timer_get();
 
-        seconds_elapsed = (time_stamp - Remote_PauseTimeStamp) / (TIMER_FPS_TO_TICKS(1) - 1);
+        seconds_elapsed = (time_stamp - Remote_PauseTimeStamp) / (TIMER_FPS_TO_MS(1) - 1);
 
         if (seconds_elapsed) {
             Remote_PauseTimeStamp = time_stamp;
@@ -5083,7 +5083,7 @@ void GameManager_ProcessState(bool process_tick, bool clear_mouse_events) {
             }
 
         } else {
-            Paths_LastTimeStamp = timer_get_stamp32();
+            Paths_LastTimeStamp = timer_get();
             Paths_TimeBenchmarkDisable = false;
         }
 
@@ -5120,7 +5120,7 @@ bool GameManager_ProcessTick(bool render_screen) {
 
     GameManager_AdvanceFlic();
 
-    time_stamp = timer_get_stamp32();
+    time_stamp = timer_get();
 
     for (int i = sizeof(GameManager_ColorCycleTable) / sizeof(struct ColorCycleData) - 1; i >= 0; --i) {
         ColorCycleData* data;
@@ -5133,9 +5133,9 @@ bool GameManager_ProcessTick(bool render_screen) {
         }
     }
 
-    time_stamp = timer_get_stamp32();
+    time_stamp = timer_get();
 
-    if ((time_stamp - Paths_LastTimeStamp) >= TIMER_FPS_TO_TICKS(24)) {
+    if ((time_stamp - Paths_LastTimeStamp) >= TIMER_FPS_TO_MS(24)) {
         if (GameManager_IsCheater && GameManager_PlayMode != PLAY_MODE_UNKNOWN) {
             GameManager_PunishCheater();
         }
@@ -6862,9 +6862,9 @@ void GameManager_DrawFlic(Rect* bounds) {
 
 void GameManager_AdvanceFlic() {
     if (GameManager_Flic.flc && GameManager_PlayFlic && ini_get_setting(INI_EFFECTS)) {
-        unsigned int time_stamp = timer_get_stamp32();
+        unsigned int time_stamp = timer_get();
 
-        if ((time_stamp - GameManager_FlicFrameTimeStamp) >= TIMER_FPS_TO_TICKS(15)) {
+        if ((time_stamp - GameManager_FlicFrameTimeStamp) >= TIMER_FPS_TO_MS(15)) {
             GameManager_FlicFrameTimeStamp = time_stamp;
 
             // the original design assumes that animated FLICs do not redraw the top text rows

src/gamesetupmenu.cpp
@@ -231,12 +231,12 @@ void GameSetupMenu::EventBriefingButton() {
             }
 
             do {
-                time_stamp = timer_get_stamp32();
+                time_stamp = timer_get();
 
                 --string_row_index;
                 DrawDescriptionPanel();
 
-                while (timer_get_stamp32() - time_stamp < TIMER_FPS_TO_TICKS(96)) {
+                while (timer_get() - time_stamp < TIMER_FPS_TO_MS(96)) {
                 }
 
             } while (string_row_index != row_index);
@@ -250,12 +250,12 @@ void GameSetupMenu::EventBriefingButton() {
             row_index = string_row_index + rows_per_page;
 
             do {
-                time_stamp = timer_get_stamp32();
+                time_stamp = timer_get();
 
                 ++string_row_index;
                 DrawDescriptionPanel();
 
-                while (timer_get_stamp32() - time_stamp < TIMER_FPS_TO_TICKS(96)) {
+                while (timer_get() - time_stamp < TIMER_FPS_TO_MS(96)) {
                 }
 
             } while (string_row_index != row_index);

src/gnw.c
@@ -130,7 +130,7 @@ int win_init(SetModeFunc set, ResetModeFunc reset, int flags) {
         free(pal);
     }
 
-    GNW_input_init((flags & 2) == 0);
+    GNW_input_init();
     GNW_intr_init();
 
     window[0] = (GNW_Window *)malloc(sizeof(GNW_Window));

src/helpmenu.cpp
@@ -416,12 +416,12 @@ bool HelpMenu::ProcessKey(int key) {
                 }
 
                 do {
-                    time_stamp = timer_get_stamp32();
+                    time_stamp = timer_get();
 
                     --string_row_index;
                     DrawText();
 
-                    while ((timer_get_stamp32() - time_stamp) < TIMER_FPS_TO_TICKS(96)) {
+                    while ((timer_get() - time_stamp) < TIMER_FPS_TO_MS(96)) {
                     }
 
                 } while (string_row_index != row_index);
@@ -438,12 +438,12 @@ bool HelpMenu::ProcessKey(int key) {
                 row_index = string_row_index + rows_per_page;
 
                 do {
-                    time_stamp = timer_get_stamp32();
+                    time_stamp = timer_get();
 
                     ++string_row_index;
                     DrawText();
 
-                    while ((timer_get_stamp32() - time_stamp) < TIMER_FPS_TO_TICKS(96)) {
+                    while ((timer_get() - time_stamp) < TIMER_FPS_TO_MS(96)) {
                     }
 
                 } while (string_row_index != row_index);

src/input.c
@@ -57,7 +57,6 @@ static int input_my;
 static FuncPtr bk_list;
 static int game_paused;
 static int screendump_key;
-static int using_msec_timer;
 static int pause_key;
 static ScreenDumpFunc screendump_func;
 static int input_get;
@@ -66,7 +65,7 @@ static PauseWinFunc pause_win_func;
 static int input_put;
 static int bk_disabled;
 
-int GNW_input_init(int use_msec_timer) {
+int GNW_input_init(void) {
     int result;
 
     bk_list = NULL;
@@ -87,11 +86,6 @@ int GNW_input_init(int use_msec_timer) {
 
     screendump_key = 302;
     screendump_func = default_screendump;
-
-    using_msec_timer = use_msec_timer;
-    if (use_msec_timer) {
-        timer_set_rate();
-    }
 
     return result;
 }

commit 943c91ae8f173cd51b19a2984c80939f764fa872

Fixed regression from ea5ac85.

src/taskclearzone.cpp
@@ -175,7 +175,7 @@ void TaskClearZone::EndTurn() {
 
                 if (state == CLEARZONE_STATE_WAITING) {
                     while (!ExamineZones()) {
-                        if (TickTimer_HaveTimeToThink()) {
+                        if (!TickTimer_HaveTimeToThink()) {
                             state = CLEARZONE_STATE_WAITING;
 
                             RemindTurnEnd(true);