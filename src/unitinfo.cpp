klei1984/max

commit 2f7f55ae614783202f94fc0d2b96ae8dc1093678

Encapsulate unit_type member of UnitInfo class.

src/unitinfo.cpp
@@ -473,23 +473,88 @@ void UnitInfo_BuildList_FileSave(SmartObjectArray<ResourceID>* build_list, Smart
 }
 
 UnitInfo::UnitInfo()
-    : unit_type(INVALID_ID),
+    : name(nullptr),
+      sound(SFX_TYPE_INVALID),
+      unit_type(INVALID_ID),
       popup(nullptr),
       sound_table(nullptr),
+      flags(0),
+      x(-1),
+      y(-1),
+      grid_x(-1),
+      grid_y(-1),
+      color_cycling_lut(nullptr),
+      team(-1),
+      unit_id(0),
+      brightness(UINT8_MAX),
+      angle(0),
+      max_velocity(0),
+      velocity(0),
+      scaler_adjust(0),
+      turret_angle(0),
+      turret_offset_x(0),
+      turret_offset_y(0),
+      total_images(0),
+      image_base(0),
+      turret_image_base(0),
+      firing_image_base(0),
+      connector_image_base(0),
+      image_index_max(0),
+      orders(ORDER_AWAIT),
+      state(ORDER_STATE_INIT),
+      prior_orders(ORDER_AWAIT),
+      prior_state(ORDER_STATE_INIT),
+      target_grid_x(0),
+      target_grid_y(0),
+      build_time(0),
+      total_mining(0),
+      raw_mining(0),
+      fuel_mining(0),
+      gold_mining(0),
+      raw_mining_max(0),
+      gold_mining_max(0),
+      fuel_mining_max(0),
+      hits(0),
+      speed(0),
+      group_speed(0),
+      shots(0),
+      move_and_fire(0),
+      storage(0),
+      ammo(0),
+      targeting_mode(0),
+      enter_mode(0),
+      cursor(0),
+      recoil_delay(0),
+      delayed_reaction(0),
+      damaged_this_turn(false),
+      disabled_reaction_fire(false),
+      auto_survey(false),
+      research_topic(false),
+      moved(0),
+      bobbed(false),
+      engine(0),
+      weapon(0),
+      comm(0),
+      fuel_distance(0),
+      move_fraction(0),
+      connectors(0),
+      shake_effect_state(0),
+      build_rate(0),
+      repeat_build(0),
+      energized(false),
+      id(0),
+      unit_list(nullptr),
       in_transit(false),
       pin_count(0),
-      field_165(true),
-      unit_list(nullptr),
-      group_speed(0),
-      name(nullptr),
-      sound(SFX_TYPE_INVALID),
-      build_rate(1),
-      field_221(0),
-      disabled_reaction_fire(false),
-      auto_survey(false) {
-    rect_init(&sprite_bounds, 0, 0, 0, 0);
-    rect_init(&shadow_bounds, 0, 0, 0, 0);
-}
+      field_165(false),
+      laying_state(0),
+      visible_to_team(),
+      spotted_by_team(),
+      sprite_bounds(),
+      shadow_bounds(),
+      image_index(0),
+      turret_image_index(0),
+      field_221(0) {}
 
 UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t angle)
     : orders(ORDER_AWAIT),
@@ -5145,4 +5210,12 @@ uint8_t UnitInfo::SetSfxType(uint8_t sound) noexcept {
     this->sound = sound;
 
     return previous_sound;
+}
+
+[[nodiscard]] ResourceID UnitInfo::GetUnitType() const noexcept { return unit_type; }
+
+void UnitInfo::SetUnitType(const ResourceID unit_type) noexcept {
+    SDL_assert(unit_type < UNIT_END);
+
+    this->unit_type = unit_type;
 }

src/unitinfo.hpp
@@ -90,19 +90,87 @@ private:
     SmartPointer<UnitInfo> parent_unit;
     char* name;
     uint8_t sound;
+    ResourceID unit_type;
 
 public:
+    /**
+     * The default constructor is only usable in special use cases.
+     */
     UnitInfo();
+
+    /**
+     * Construct a particular unit type for a given team.
+     *
+     * \param unit_type one of the ResourceID enum values below UNIT_END
+     * \param team unit one of the PlayerTeam enum values
+     * \param id unit hash between 0x0000 - 0xFFFE, 0xFFFF is the invalid id
+     * \param angle one of the UnitAngle enum values
+     */
     UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t angle = 0);
+
+    /** Copy constructor. */
     UnitInfo(const UnitInfo& other);
+
+    /** Destructor. */
     ~UnitInfo();
 
-    static FileObject* Allocate() noexcept;
+    /**
+     * Get UTF-8 encoded null terminated name of unit without mark segment.
+     *
+     * \param text a plain char* buffer to hold the returned unit name
+     * \param size buffer size in bytes
+     */
+    void GetName(char* const text, const size_t size) const noexcept;
 
+    /**
+     * Get UTF-8 encoded null terminated full name of unit.
+     *
+     * \param text a plain char* buffer to hold the returned unit name
+     * \param size buffer size in bytes
+     */
+    void GetDisplayName(char* const text, const size_t size) const noexcept;
+
+    /**
+     * Set UTF-8 encoded null terminated name of unit without mark segment.
+     *
+     * \param text a plain char* buffer that holds the name to set
+     */
+    void SetName(const char* const text) noexcept;
+
+    /**
+     * Get sound effect type being played by the sound manager for the unit.
+     * SFX_TYPE_INVALID means no sound effect is associated with the unit.
+     *
+     * \returns one of the SfxType enum values
+     */
+    [[nodiscard]] uint8_t GetSfxType() const noexcept;
+
+    /**
+     * Set sound effect type being played by the sound manager for the unit.
+     *
+     * \param sound one of the SfxType enum values
+     * \returns previous sound type
+     */
+    uint8_t SetSfxType(uint8_t sound) noexcept;
+
+    /**
+     * Get type of unit.
+     *
+     * \returns one of the ResourceID enum values below UNIT_END
+     */
+    [[nodiscard]] ResourceID GetUnitType() const noexcept;
+
+    /**
+     * Set type of unit.
+     *
+     * \param unit_type one of the ResourceID enum values below UNIT_END
+     */
+    void SetUnitType(ResourceID unit_type) noexcept;
+
+    static FileObject* Allocate() noexcept;
     uint16_t GetTypeIndex() const;
     void FileLoad(SmartFileReader& file) noexcept;
     void FileSave(SmartFileWriter& file) noexcept;
-
     static int32_t GetDrawLayer(ResourceID unit_type);
     bool IsVisibleToTeam(uint16_t team) const;
     void SetEnemy(UnitInfo* enemy);
@@ -159,29 +227,6 @@ public:
     void StopMovement();
     void SpotByTeam(uint16_t team);
     static void GetVersion(char* text, int32_t version);
-
-    /**
-     * Get UTF-8 encoded null terminated name of unit without mark segment.
-     *
-     * \param text a plain char* buffer to hold the returned unit name
-     * \param size buffer size in bytes
-     */
-    void GetName(char* const text, const size_t size) const noexcept;
-
-    /**
-     * Get UTF-8 encoded null terminated full name of unit.
-     *
-     * \param text a plain char* buffer to hold the returned unit name
-     * \param size buffer size in bytes
-     */
-    void GetDisplayName(char* const text, const size_t size) const noexcept;
-
-    /**
-     * Set UTF-8 encoded null terminated name of unit without mark segment.
-     *
-     * \param text a plain char* buffer that holds the name to set
-     */
-    void SetName(const char* const text) noexcept;
     int32_t GetRaw();
     int32_t GetRawFreeCapacity();
     void TransferRaw(int32_t amount);
@@ -250,23 +295,6 @@ public:
     [[nodiscard]] UnitInfo* GetParent() const noexcept;
     void SetParent(UnitInfo* const parent) noexcept;
 
-    /**
-     * Get sound effect type being played by the sound manager for the unit.
-     * SFX_TYPE_INVALID means no sound effect is associated with the unit.
-     *
-     * \returns one of the SFX_TYPE_* enum values.
-     */
-    [[nodiscard]] uint8_t GetSfxType() const noexcept;
-
-    /**
-     * Set sound effect type being played by the sound manager for the unit.
-     *
-     * \param sound one of the SFX_TYPE_* enum values.
-     * \returns previous sound type.
-     */
-    uint8_t SetSfxType(uint8_t sound) noexcept;
-
-    ResourceID unit_type;
     struct PopupFunctions* popup;
     const std::vector<SoundElement>* sound_table;
     uint32_t flags;

commit acc8d07971c5494f6bf699aa262a02e80e89b792

Fixed regression from 2f7f55a (fixes #47).

src/unitinfo.cpp
@@ -546,7 +546,7 @@ UnitInfo::UnitInfo()
       unit_list(nullptr),
       in_transit(false),
       pin_count(0),
-      field_165(false),
+      tasks_enabled(true),
       laying_state(0),
       visible_to_team(),
       spotted_by_team(),
@@ -631,7 +631,7 @@ UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t ang
       unit_list(nullptr),
       in_transit(false),
       pin_count(0),
-      field_165(true),
+      tasks_enabled(true),
       laying_state(0),
       visible_to_team(),
       spotted_by_team(),
@@ -784,7 +784,7 @@ UnitInfo::UnitInfo(const UnitInfo& other)
       in_transit(other.in_transit),
       last_target(other.last_target),
       pin_count(other.pin_count),
-      field_165(other.field_165),
+      tasks_enabled(other.tasks_enabled),
       laying_state(other.laying_state),
       name(nullptr),
       sprite_bounds(other.sprite_bounds),
@@ -1295,13 +1295,13 @@ void UnitInfo::AddTask(Task* task) {
 }
 
 void UnitInfo::ScheduleDelayedTasks(bool priority) {
-    if (field_165) {
+    if (tasks_enabled) {
         for (SmartList<Task>::Iterator it = delayed_tasks.Begin(); it != delayed_tasks.End(); ++it) {
             (*it).RemindTurnEnd(true);
         }
     }
 
-    if (GetTask() && field_165) {
+    if (GetTask() && tasks_enabled) {
         Task_RemindMoveFinished(this, priority);
     }
 }
@@ -4795,16 +4795,16 @@ bool UnitInfo::Upgrade(UnitInfo* parent) {
 }
 
 void UnitInfo::BusyWaitOrder() {
-    bool last_state = field_165;
+    bool last_state = tasks_enabled;
 
-    field_165 = false;
+    tasks_enabled = false;
 
     while (orders != ORDER_AWAIT) {
         GameManager_ProcessTick(false);
         MouseEvent::ProcessInput();
     }
 
-    field_165 = last_state;
+    tasks_enabled = last_state;
 }
 
 void UnitInfo::PositionInTape() {

src/unitinfo.hpp
@@ -434,7 +434,7 @@ public:
     bool in_transit;
     Point last_target;
     int16_t pin_count;
-    bool field_165;
+    bool tasks_enabled;
     uint8_t laying_state;
     uint8_t visible_to_team[PLAYER_TEAM_MAX];
     uint8_t spotted_by_team[PLAYER_TEAM_MAX];