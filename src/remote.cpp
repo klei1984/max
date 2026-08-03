klei1984/max

commit 23df14f4b5d61f5c4549733f5aa7f056302dbb07

Fixed various regressions. Split more multi-purpose unit info fields.

src/remote.cpp
@@ -21,6 +21,8 @@
 
 #include "remote.hpp"
 
+#include <format>
+
 #include "access.hpp"
 #include "ailog.hpp"
 #include "cursor.hpp"
@@ -85,6 +87,9 @@ struct Packet23Data {
     uint8_t speed;
     uint8_t shots;
     int16_t storage;
+    int16_t experience;
+    int16_t transfer_cargo;
+    uint8_t stealth_dice_roll;
     uint8_t ammo;
 };
 
@@ -131,6 +136,8 @@ static void Remote_ClearNotifyTimeout(bool main_menu_state);
 static void Remote_WriteGameSettings(NetPacket& packet, const std::shared_ptr<Mission> mission);
 static void Remote_ReadGameSettings(NetPacket& packet);
 
+static void Remote_OrderProcessor0_Write(UnitInfo* unit, NetPacket& packet);
+static void Remote_OrderProcessor0_Read(UnitInfo* unit, NetPacket& packet);
 static void Remote_OrderProcessor1_Write(UnitInfo* unit, NetPacket& packet);
 static void Remote_OrderProcessor1_Read(UnitInfo* unit, NetPacket& packet);
 static void Remote_OrderProcessor2_Write(UnitInfo* unit, NetPacket& packet);
@@ -316,6 +323,10 @@ void Remote_ReadGameSettings(NetPacket& packet) {
     packet >> Remote_NetworkMenu->rng_seed;
 }
 
+void Remote_OrderProcessor0_Write(UnitInfo* unit, NetPacket& packet) {}
+
+void Remote_OrderProcessor0_Read(UnitInfo* unit, NetPacket& packet) {}
+
 void Remote_OrderProcessor1_Write(UnitInfo* unit, NetPacket& packet) {
     packet << unit->GetOrder();
     packet << unit->GetOrderState();
@@ -353,6 +364,8 @@ void Remote_OrderProcessor2_Write(UnitInfo* unit, NetPacket& packet) {
     } else {
         packet << static_cast<uint16_t>(0);
     }
+
+    packet << unit->experience;
 }
 
 void Remote_OrderProcessor2_Read(UnitInfo* unit, NetPacket& packet) {
@@ -368,6 +381,8 @@ void Remote_OrderProcessor2_Read(UnitInfo* unit, NetPacket& packet) {
     } else {
         unit->SetParent(nullptr);
     }
+
+    packet >> unit->experience;
 }
 
 void Remote_OrderProcessor3_Write(UnitInfo* unit, NetPacket& packet) {
@@ -382,6 +397,9 @@ void Remote_OrderProcessor3_Write(UnitInfo* unit, NetPacket& packet) {
     } else {
         packet << static_cast<uint16_t>(0);
     }
+
+    packet << unit->transfer_cargo;
+    packet << unit->stealth_dice_roll;
 }
 
 void Remote_OrderProcessor3_Read(UnitInfo* unit, NetPacket& packet) {
@@ -399,6 +417,9 @@ void Remote_OrderProcessor3_Read(UnitInfo* unit, NetPacket& packet) {
     } else {
         unit->SetEnemy(nullptr);
     }
+
+    packet >> unit->transfer_cargo;
+    packet >> unit->stealth_dice_roll;
 }
 
 void Remote_OrderProcessor4_Write(UnitInfo* unit, NetPacket& packet) {
@@ -566,6 +587,12 @@ void Remote_Init() {
 
     for (int32_t i = 0; i < ORDER_COUNT_MAX; ++i) {
         switch (i) {
+            case ORDER_17: {
+                // unused order slots
+                Remote_OrderProcessors[i].WritePacket = &Remote_OrderProcessor0_Write;
+                Remote_OrderProcessors[i].ReadPacket = &Remote_OrderProcessor0_Read;
+            } break;
+
             case ORDER_POWER_ON:
             case ORDER_POWER_OFF:
             case ORDER_EXPLODE:
@@ -1042,12 +1069,12 @@ void Remote_NetErrorUnknownUnit(uint16_t unit_id) {
 }
 
 void Remote_NetErrorUnitInfoOutOfSync(UnitInfo* unit, NetPacket& packet) {
-    char message[100];
-    AILOG(log, "Units are out of sync:  Host, Peer");
     struct Packet23Data data;
     const char* const team_names[PLAYER_TEAM_MAX + 1] = {"Red", "Green", "Blue", "Gray", "Neutral", "Unknown"};
 
     Remote_ProcessNetPacket_23(data, packet);
+
+    AILOG(log, "Units are out of sync:  Host, Peer");
 
     AILOG_LOG(log, " team          {}, {}",
               (unit->team < PLAYER_TEAM_MAX) ? team_names[unit->team] : team_names[PLAYER_TEAM_MAX],
@@ -1085,11 +1112,14 @@ void Remote_NetErrorUnitInfoOutOfSync(UnitInfo* unit, NetPacket& packet) {
     AILOG_LOG(log, " speed         {}, {}", unit->speed, data.speed);
     AILOG_LOG(log, " rounds        {}, {}", unit->shots, data.shots);
     AILOG_LOG(log, " storage       {}, {}", unit->storage, data.storage);
+    AILOG_LOG(log, " experience    {}, {}", unit->experience, data.experience);
+    AILOG_LOG(log, " x-fer cargo   {}, {}", unit->transfer_cargo, data.transfer_cargo);
+    AILOG_LOG(log, " stealth dice  {}, {}", unit->stealth_dice_roll, data.stealth_dice_roll);
     AILOG_LOG(log, " ammo          {}, {}", unit->ammo, data.ammo);
 
-    sprintf(message, "Unit, id %i, is in different state in remote packet.", unit->GetId());
+    std::string message = std::format("Unit, id {}, is in different state in remote packet.", unit->GetId());
 
-    MessageManager_DrawMessage(message, 2, 1, false, true);
+    MessageManager_DrawMessage(message.c_str(), 2, 1, false, true);
 }
 
 void Remote_AnalyzeDesync() {
@@ -2435,6 +2465,9 @@ void Remote_ProcessNetPacket_23(struct Packet23Data& data, NetPacket& packet) {
     local >> data.speed;
     local >> data.shots;
     local >> data.storage;
+    local >> data.experience;
+    local >> data.transfer_cargo;
+    local >> data.stealth_dice_roll;
     local >> data.ammo;
 }
 

src/aiattack.cpp
@@ -387,7 +387,7 @@ bool AiAttack_ProcessAttack(UnitInfo* attacker, UnitInfo* target) {
                     Ai_SetReactionState(AI_REACTION_STATE_ANIMATIONS_ACTIVE);
 
                     attacker->SetParent(target);
-                    attacker->target_grid_x = Randomizer_Generate(101);
+                    attacker->stealth_dice_roll = Randomizer_Generate(101);
 
                     if (target->GetOrder() == ORDER_DISABLE ||
                         (!(target->flags & STATIONARY) &&

src/enums.hpp
@@ -86,6 +86,7 @@ enum UnitOrderType : uint8_t {
     ORDER_LOAD = 0xF,
     ORDER_IDLE = 0x10,
     ORDER_REPAIR = 0x11,
+    ORDER_17,
     ORDER_RELOAD = 0x13,
     ORDER_TRANSFER = 0x14,
     ORDER_HALT_BUILDING = 0x15,

src/game_manager.cpp
@@ -4580,7 +4580,7 @@ void GameManager_TransferCargo(UnitInfo* unit1, UnitInfo* unit2) {
         if (cargo_transferred) {
             char message[200];
 
-            unit1->target_grid_x = cargo_transferred;
+            unit1->transfer_cargo = cargo_transferred;
 
             UnitsManager_SetNewOrder(unit1, ORDER_TRANSFER, ORDER_STATE_INIT);
 
@@ -4613,7 +4613,7 @@ void GameManager_StealUnit(UnitInfo* unit1, UnitInfo* unit2) {
     if (unit2) {
         if (unit1->shots > 0) {
             unit1->SetParent(unit2);
-            unit1->target_grid_x = Randomizer_Generate(101);
+            unit1->stealth_dice_roll = Randomizer_Generate(101);
 
             UnitsManager_SetNewOrder(unit1, ORDER_AWAIT_STEAL_UNIT, ORDER_STATE_INIT);
 
@@ -4629,7 +4629,7 @@ void GameManager_DisableUnit(UnitInfo* unit1, UnitInfo* unit2) {
     if (unit2) {
         if (unit1->shots > 0) {
             unit1->SetParent(unit2);
-            unit1->target_grid_x = Randomizer_Generate(101);
+            unit1->stealth_dice_roll = Randomizer_Generate(101);
 
             UnitsManager_SetNewOrder(unit1, ORDER_AWAIT_DISABLE_UNIT, ORDER_STATE_INIT);
 

src/taskgetmaterials.cpp
@@ -138,21 +138,21 @@ void TaskGetMaterials::DoTransfer() {
 
             if (materials.raw >= requestor->GetBaseValues()->GetAttribute(ATTRIB_STORAGE) * 2 ||
                 materials.raw == capacity.raw) {
-                source->target_grid_x = requestor->GetBaseValues()->GetAttribute(ATTRIB_STORAGE) - requestor->storage;
+                source->transfer_cargo = requestor->GetBaseValues()->GetAttribute(ATTRIB_STORAGE) - requestor->storage;
 
             } else {
-                source->target_grid_x = materials_needed - requestor->storage;
+                source->transfer_cargo = materials_needed - requestor->storage;
             }
 
-            if (source->target_grid_x > materials.raw) {
-                source->target_grid_x = materials.raw;
+            if (source->transfer_cargo > materials.raw) {
+                source->transfer_cargo = materials.raw;
             }
 
         } else {
-            source->target_grid_x = materials_needed - requestor->storage;
+            source->transfer_cargo = materials_needed - requestor->storage;
 
-            if (source->target_grid_x > source->storage) {
-                source->target_grid_x = source->storage;
+            if (source->transfer_cargo > source->storage) {
+                source->transfer_cargo = source->storage;
             }
         }
 
@@ -161,7 +161,7 @@ void TaskGetMaterials::DoTransfer() {
         SDL_assert(GameManager_IsActiveTurn(m_team));
 
         AILOG_LOG(log, "Order {} materials from {} at [{},{}] for {} at [{},{}] holding {} materials.",
-                  source->target_grid_x, UnitsManager_BaseUnits[source->GetUnitType()].GetSingularName(),
+                  source->transfer_cargo, UnitsManager_BaseUnits[source->GetUnitType()].GetSingularName(),
                   source->grid_x + 1, source->grid_y + 1,
                   UnitsManager_BaseUnits[requestor->GetUnitType()].GetSingularName(), requestor->grid_x + 1,
                   requestor->grid_y + 1, requestor->storage);

src/unitinfo.cpp
@@ -521,6 +521,8 @@ UnitInfo::UnitInfo()
       move_and_fire(0),
       storage(0),
       experience(0),
+      transfer_cargo(0),
+      stealth_dice_roll(0),
       ammo(0),
       targeting_mode(0),
       enter_mode(0),
@@ -603,6 +605,8 @@ UnitInfo::UnitInfo(ResourceID unit_type, uint16_t team, uint16_t id, uint8_t ang
       move_and_fire(0),
       storage(0),
       experience(0),
+      transfer_cargo(0),
+      stealth_dice_roll(0),
       ammo(0),
       targeting_mode(0),
       enter_mode(0),
@@ -746,6 +750,8 @@ UnitInfo::UnitInfo(const UnitInfo& other)
       move_and_fire(other.move_and_fire),
       storage(other.storage),
       experience(other.experience),
+      transfer_cargo(other.transfer_cargo),
+      stealth_dice_roll(other.stealth_dice_roll),
       ammo(other.ammo),
       targeting_mode(other.targeting_mode),
       enter_mode(other.enter_mode),
@@ -3041,8 +3047,13 @@ void UnitInfo::FileLoad(SmartFileReader& file) noexcept {
             experience = 0;
         }
 
+        transfer_cargo = target_grid_x;
+        stealth_dice_roll = static_cast<uint8_t>(target_grid_x);
+
     } else {
         file.Read(experience);
+        file.Read(transfer_cargo);
+        file.Read(stealth_dice_roll);
     }
 
     file.Read(ammo);
@@ -3179,6 +3190,8 @@ void UnitInfo::FileSave(SmartFileWriter& file) noexcept {
     file.Write(move_and_fire);
     file.Write(storage);
     file.Write(experience);
+    file.Write(transfer_cargo);
+    file.Write(stealth_dice_roll);
     file.Write(ammo);
     file.Write(targeting_mode);
     file.Write(enter_mode);

src/unitinfo.hpp
@@ -404,6 +404,8 @@ public:
     uint8_t move_and_fire;
     int16_t storage;
     int16_t experience;
+    int16_t transfer_cargo;
+    uint8_t stealth_dice_roll;
     uint8_t ammo;
     uint8_t targeting_mode;
     uint8_t enter_mode;

src/units_manager.cpp
@@ -6163,7 +6163,7 @@ void UnitsManager_Transfer(UnitInfo* unit) {
     SmartPointer<UnitInfo> source(unit);
     SmartPointer<UnitInfo> target(unit->GetParent());
     int32_t cargo_type = UnitsManager_BaseUnits[unit->GetUnitType()].cargo_type;
-    int32_t transfer_amount = unit->target_grid_x;
+    int32_t transfer_amount = unit->transfer_cargo;
 
     if (transfer_amount < 0) {
         source = target;
@@ -6269,7 +6269,7 @@ bool UnitsManager_AttemptStealthAction(UnitInfo* unit) {
             }
         }
 
-        if (unit->target_grid_x <= stealth_chance) {
+        if (unit->stealth_dice_roll <= stealth_chance) {
             if (parent->GetOrder() != ORDER_DISABLE) {
                 ++unit->experience;
             }

commit 00cff6277a425bf4b8df2c1dedb7e6e11667b8f2

Fixed regression from 23df14f.

src/remote.cpp
@@ -1087,7 +1087,9 @@ void Remote_NetErrorUnitInfoOutOfSync(UnitInfo* unit, NetPacket& packet) {
     AILOG_LOG(
         log, " type          {}, {}",
         (unit->GetUnitType() < UNIT_END) ? ResourceManager_GetUnit(unit->GetUnitType()).GetSingularName().data() : "?",
-        (data.unit_type < UNIT_END) ? ResourceManager_GetUnit(unit->GetUnitType()).GetSingularName().data() : "?");
+        (data.unit_type < UNIT_END)
+            ? ResourceManager_GetUnit(static_cast<ResourceID>(data.unit_type)).GetSingularName().data()
+            : "?");
 
     AILOG_LOG(log, " unit id       {}, {}", unit->unit_id, data.unit_id);
     AILOG_LOG(log, " parent id     {}, {}", unit->GetParent() ? unit->GetParent()->GetId() : 0, data.parent_unit_id);
@@ -2451,12 +2453,16 @@ void Remote_ProcessNetPacket_23(struct Packet23Data& data, NetPacket& packet) {
     local >> data.disabled_reaction_fire;
 
     local >> data.parent_unit_id;
+    local >> data.experience;
 
     local >> data.move_to_grid_x;
     local >> data.move_to_grid_y;
     local >> data.fire_on_grid_x;
     local >> data.fire_on_grid_y;
     local >> data.enemy_unit_id;
+
+    local >> data.transfer_cargo;
+    local >> data.stealth_dice_roll;
 
     local >> data.total_mining;
     local >> data.raw_mining;
@@ -2474,9 +2480,6 @@ void Remote_ProcessNetPacket_23(struct Packet23Data& data, NetPacket& packet) {
     local >> data.speed;
     local >> data.shots;
     local >> data.storage;
-    local >> data.experience;
-    local >> data.transfer_cargo;
-    local >> data.stealth_dice_roll;
     local >> data.ammo;
 }
 