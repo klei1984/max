klei1984/max

commit 0aaaffe4c5e26e55dfb94e8c3df94d25eabc9c4e

Integrated settings.json handling. Removed INI file handler modules.

src/transport_udp_default.cpp
@@ -32,8 +32,9 @@
 
 #include <utility>
 
-#include "inifile.hpp"
 #include "netlog.hpp"
+#include "resource_manager.hpp"
+#include "settings.hpp"
 #include "version.hpp"
 
 enum : uint8_t {
@@ -144,14 +145,13 @@ TransportUdpDefault::~TransportUdpDefault() {
 }
 
 void TransportUdpDefault_GetServerAddress(ENetAddress& address) {
-    char server_address[30];
+    std::string server_address = ResourceManager_GetSettings()->GetStringValue("server_address");
 
-    if (!ini_config.GetStringValue(INI_NETWORK_HOST_ADDRESS, server_address, sizeof(server_address)) ||
-        enet_address_set_host_ip(&address, server_address) != 0) {
+    if (server_address.empty() || enet_address_set_host_ip(&address, server_address.c_str()) != 0) {
         (void)enet_address_set_host_ip(&address, "127.0.0.1");
     }
 
-    int32_t server_port = ini_get_setting(INI_NETWORK_HOST_PORT);
+    int32_t server_port = ResourceManager_GetSettings()->GetNumericValue("host_port");
 
     if (!server_port || server_port < 1024 || server_port > 65535) {
         server_port = TransportUdpDefault_DefaultHostPort;
@@ -181,7 +181,7 @@ bool TransportUdpDefault::Init(int32_t mode) {
         context->NetRole = -1;
         context->LastError = "No error.";
 
-        if (ini_get_setting(INI_LOG_FILE_DEBUG)) {
+        if (ResourceManager_GetSettings()->GetNumericValue("log_file_debug")) {
             NetLog_Enable();
         }
     }

commit 17e179cc7bb13d9f49a0aa36c3beab00fdab6162

Fixed regression from 0aaaffe.

src/transport_udp_default.cpp
@@ -146,7 +146,7 @@ TransportUdpDefault::~TransportUdpDefault() {
 }
 
 void TransportUdpDefault_GetServerAddress(ENetAddress& address) {
-    std::string server_address = ResourceManager_GetSettings()->GetStringValue("server_address");
+    std::string server_address = ResourceManager_GetSettings()->GetStringValue("host_address");
 
     if (server_address.empty() || enet_address_set_host_ip(&address, server_address.c_str()) != 0) {
         (void)enet_address_set_host_ip(&address, "127.0.0.1");