klei1984/max

commit 8316eb2d34d5d55b2e67c225cc700adbf922aa7b

Integrated clans component.

src/cargomenu.cpp
@@ -24,6 +24,7 @@
 #include "helpmenu.hpp"
 #include "inifile.hpp"
 #include "menu.hpp"
+#include "resource_manager.hpp"
 #include "text.hpp"
 #include "units_manager.hpp"
 #include "window_manager.hpp"
@@ -38,9 +39,15 @@ CargoMenu::CargoMenu(uint16_t team) : AbstractUpgradeMenu(team, CARGOPIC) {
     WindowInfo wininfo1;
     WindowInfo wininfo2;
 
-    start_gold = ini_get_setting(INI_START_GOLD) + ini_clans.GetClanGold(UnitsManager_TeamInfo[team].team_clan);
+    auto clans = ResourceManager_GetClans();
+    const auto clan_id = ResourceManager_GetClanID(static_cast<TeamClanType>(UnitsManager_TeamInfo[team].team_clan));
 
-    UnitsManager_TeamInfo[this->team].stats_gold_spent_on_upgrades = 0;
+    start_gold = ini_get_setting(INI_START_GOLD);
+
+    if (clans) {
+        start_gold += clans->GetCredits(clan_id);
+    }
+
     interface_icon_full = I_GOLD;
     interface_icon_empty = I_GOLDE;
 
@@ -48,20 +55,27 @@ CargoMenu::CargoMenu(uint16_t team) : AbstractUpgradeMenu(team, CARGOPIC) {
     cargos = UnitsManager_TeamMissionSupplies[team].cargos;
 
     if (unit_types2->GetCount()) {
-        SmartObjectArray<ResourceID> default_units;
-
-        UnitsManager_AddAxisMissionLoadout(team, default_units);
-
-        unit_count = default_units.GetCount() + 3;
         team_gold = UnitsManager_TeamMissionSupplies[team].team_gold;
+        unit_count = UnitsManager_TeamMissionSupplies[team].units.GetCount();
 
-        for (int32_t i = 0; i < UNIT_END; ++i) {
+        for (int32_t i = UNIT_START; i < UNIT_END; ++i) {
             unitvalues_base[i] = UnitsManager_TeamInfo[team].team_units->GetBaseUnitValues(i);
         }
 
     } else {
-        unit_count = UnitsManager_AddDefaultMissionLoadout(team);
+        UnitsManager_TeamMissionSupplies[team].team_gold = start_gold;
+        UnitsManager_TeamInfo[team].stats_gold_spent_on_upgrades = 0;
+
         team_gold = start_gold;
+        unit_count = 0;
+
+        auto mission_manager = ResourceManager_GetMissionManager();
+        if (mission_manager && mission_manager->GetGameRulesHandler()) {
+            if (mission_manager->GetGameRulesHandler()->GetMissionLoadout(team, clan_id,
+                                                                          UnitsManager_TeamMissionSupplies[team])) {
+                unit_count = UnitsManager_TeamMissionSupplies[team].units.GetCount();
+            }
+        }
     }
 
     stats_background = new (std::nothrow) Image(11, 293, 250, 174);

src/aiplayer.cpp
@@ -1344,33 +1344,26 @@ void AiPlayer::RollTeamMissionSupplies(int32_t clan) {
     ResourceID unit_type;
     uint16_t cargo;
 
-    mission_supplies->team_gold =
-        ini_clans.GetClanGold(UnitsManager_TeamInfo[player_team].team_clan) + ini_get_setting(INI_START_GOLD);
+    auto clans = ResourceManager_GetClans();
+    std::string clan_id =
+        ResourceManager_GetClanID(static_cast<TeamClanType>(UnitsManager_TeamInfo[player_team].team_clan));
+
+    mission_supplies->team_gold = ini_get_setting(INI_START_GOLD);
+
+    if (clans) {
+        mission_supplies->team_gold += clans->GetCredits(clan_id);
+    }
 
     mission_supplies->units.Clear();
-
-    unit_type = CONSTRCT;
-    mission_supplies->units.PushBack(&unit_type);
-
-    unit_type = ENGINEER;
-    mission_supplies->units.PushBack(&unit_type);
-
-    unit_type = SURVEYOR;
-    mission_supplies->units.PushBack(&unit_type);
-
-    UnitsManager_AddAxisMissionLoadout(player_team, mission_supplies->units);
-
     mission_supplies->cargos.Clear();
 
-    cargo = 40;
-    mission_supplies->cargos.PushBack(&cargo);
+    auto mission_manager = ResourceManager_GetMissionManager();
 
-    cargo = 20;
-    mission_supplies->cargos.PushBack(&cargo);
-
-    for (uint32_t i = 2; i < mission_supplies->units.GetCount(); ++i) {
-        cargo = 0;
-        mission_supplies->cargos.PushBack(&cargo);
+    if (mission_manager && mission_manager->GetGameRulesHandler()) {
+        if (!mission_manager->GetGameRulesHandler()->GetMissionLoadout(player_team, clan_id, *mission_supplies)) {
+            /* There is no error reporting from RollTeamMissionSupplies */
+            SDL_assert(0);
+        }
     }
 
     switch (strategy) {

src/clans.cpp
@@ -239,6 +239,33 @@ Clans::Clans()
 
 Clans::~Clans() {}
 
+Clans::KeyIterator::KeyIterator(MapIterator iter) : m_iter(iter) {}
+
+Clans::KeyIterator::reference Clans::KeyIterator::operator*() const { return m_iter->first; }
+
+Clans::KeyIterator::pointer Clans::KeyIterator::operator->() const { return &m_iter->first; }
+
+Clans::KeyIterator& Clans::KeyIterator::operator++() {
+    ++m_iter;
+    return *this;
+}
+
+Clans::KeyIterator Clans::KeyIterator::operator++(int) {
+    KeyIterator tmp = *this;
+    ++(*this);
+    return tmp;
+}
+
+bool operator==(const Clans::KeyIterator& a, const Clans::KeyIterator& b) { return a.m_iter == b.m_iter; }
+
+bool operator!=(const Clans::KeyIterator& a, const Clans::KeyIterator& b) { return a.m_iter != b.m_iter; }
+
+Clans::KeyRange::KeyRange(const std::unordered_map<std::string, ClanObject>* map) : m_map(map) {}
+
+Clans::KeyIterator Clans::KeyRange::begin() const { return KeyIterator(m_map->begin()); }
+
+Clans::KeyIterator Clans::KeyRange::end() const { return KeyIterator(m_map->end()); }
+
 [[nodiscard]] std::string Clans::LoadSchema() {
     uint32_t file_size = ResourceManager_GetResourceSize(SC_SCHEC);
     uint8_t* file_base = ResourceManager_ReadResource(SC_SCHEC);

src/clans.hpp
@@ -77,6 +77,41 @@ public:
     [[nodiscard]] int32_t GetUnitTradeoff(const std::string& clan_id, const ResourceID resource_id,
                                           const AttributeID attribute) const;
     [[nodiscard]] ResourceType GetLogo(const std::string& clan_id) const;
+
+    class KeyIterator {
+        using MapIterator = std::unordered_map<std::string, ClanObject>::const_iterator;
+        MapIterator m_iter;
+
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = std::string;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const std::string*;
+        using reference = const std::string&;
+
+        explicit KeyIterator(MapIterator iter);
+
+        reference operator*() const;
+        pointer operator->() const;
+
+        KeyIterator& operator++();
+        KeyIterator operator++(int);
+
+        friend bool operator==(const KeyIterator& a, const KeyIterator& b);
+        friend bool operator!=(const KeyIterator& a, const KeyIterator& b);
+    };
+
+    class KeyRange {
+        const std::unordered_map<std::string, ClanObject>* m_map;
+
+    public:
+        explicit KeyRange(const std::unordered_map<std::string, ClanObject>* map);
+
+        KeyIterator begin() const;
+        KeyIterator end() const;
+    };
+
+    [[nodiscard]] KeyRange GetClanKeys() const { return KeyRange(m_clans.get()); }
 };
 
 #endif /* CLANS_HPP */

src/clanselectmenu.cpp
@@ -21,9 +21,14 @@
 
 #include "clanselectmenu.hpp"
 
+#include <format>
+
 #include "helpmenu.hpp"
 #include "inifile.hpp"
 #include "menu.hpp"
+#include "resource_manager.hpp"
+#include "text.hpp"
+#include "units_manager.hpp"
 #include "window_manager.hpp"
 
 void ClanSelectMenu::Init(int32_t team) {
@@ -133,59 +138,85 @@ void ClanSelectMenu::ButtonInit(int32_t index, int32_t mode) {
 }
 
 void ClanSelectMenu::SelectMenuItems() {
-    char buffer[500];
-    char buffer2[500];
-    char* pointer;
-    int32_t color;
-    int32_t index;
+    auto clans = ResourceManager_GetClans();
 
-    for (int32_t i = 0; i < 8; ++i) {
-        ini_clans.GetClanName(i + 1, buffer, 100);
-        clan_select_menu_clan_icons[i].title = buffer;
-        color = ((i + 1) == team_clan_selection) ? COLOR_GREEN : 0xA2;
+    if (clans) {
+        for (int32_t i = 0; i < 8; ++i) {
+            const auto clan_id = ResourceManager_GetClanID(static_cast<TeamClanType>(i + 1));
+            const int32_t color = ((i + 1) == team_clan_selection) ? COLOR_GREEN : 0xA2;
 
-        menu_draw_menu_title(window, &clan_select_menu_clan_icons[i], color | GNW_TEXT_OUTLINE, true);
+            clan_select_menu_clan_icons[i].title = clans->GetName(clan_id);
+
+            menu_draw_menu_title(window, &clan_select_menu_clan_icons[i], color | GNW_TEXT_OUTLINE, true);
+        }
     }
 
     image->Write(window, WindowManager_ScaleUlx(window, clan_select_menu_screen_text[0].bounds.ulx),
                  WindowManager_ScaleUly(window, clan_select_menu_screen_text[0].bounds.uly));
-    ini_clans.GetClanText(team_clan_selection, buffer, 100);
 
-    index = 0;
-
-    DrawClanUpgrades(buffer, index++, COLOR_GREEN);
-
-    while (team_clan_selection) {
-        ini_clans.GetStringValue(buffer2, 100);
-        pointer = strstr(buffer2, "=");
-
-        if (!pointer) {
-            break;
-        }
-
-        pointer[0] = '\0';
-
-        strcpy(buffer, buffer2);
-
-        if (SDL_strcasecmp(buffer, "Name") && SDL_strcasecmp(buffer, "Text")) {
-            strcat(buffer, ": ");
-            strcat(buffer, &pointer[1]);
-
-            DrawClanUpgrades(buffer, index++, 0xA2);
-        }
+    if (clans && team_clan_selection) {
+        DrawClanUpgrades();
     }
 
     win_draw(window->id);
 }
 
-void ClanSelectMenu::DrawClanUpgrades(const char* text, int32_t index, int32_t color) {
-    const int32_t position = (index / 5) ? 1 : 0;
-    const int32_t limit1 = index % 5;
-    const int32_t limit2 = 5 - limit1;
+void ClanSelectMenu::DrawClanUpgrades() {
+    const char* attribute_names[Clans::ATTRIB_COUNT] = {
+        _(6976), _(62f5), _(d81e), _(fca3), _(4027), _(bbcc), _(2269),
+        _(206c), _(59ad), _(49a2), _(24d8), _(4a91), _(e9d8),
+    };
 
-    clan_select_menu_screen_text[position].title = std::string(limit1, '\n') + text + std::string(limit2, '\n');
+    auto clans = ResourceManager_GetClans();
+    const auto clan_id = ResourceManager_GetClanID(static_cast<TeamClanType>(team_clan_selection));
+    const auto description = clans->GetDescription(clan_id);
+    std::string unit_bonuses_text;
 
-    menu_draw_menu_title(window, &clan_select_menu_screen_text[position], color | GNW_TEXT_OUTLINE, false);
+    for (ResourceID unit_id = UNIT_START; unit_id < UNIT_END; unit_id = static_cast<ResourceID>(unit_id + 1)) {
+        std::string unit_bonuses;
+
+        for (int32_t attr = Clans::ATTRIB_TURNS_TO_BUILD; attr < Clans::ATTRIB_COUNT; ++attr) {
+            int32_t value = clans->GetUnitTradeoff(clan_id, unit_id, static_cast<Clans::AttributeID>(attr));
+
+            if (value != 0) {
+                if (!unit_bonuses.empty()) {
+                    unit_bonuses += ", ";
+                }
+
+                // use non breaking space
+                unit_bonuses += std::format("{:+}\xC2\xA0{}", value, attribute_names[attr]);
+            }
+        }
+
+        if (!unit_bonuses.empty()) {
+            const char* unit_name = UnitsManager_BaseUnits[unit_id].singular_name;
+
+            unit_bonuses_text += std::format(" {}: {}.", unit_name, unit_bonuses);
+        }
+    }
+
+    const int32_t description_width_pixels = Text_GetWidth(description.c_str());
+    const int32_t nbsp_width_pixels = Text_GetGlyphWidth(U'\u00A0');
+
+    SDL_assert(nbsp_width_pixels != 0);
+
+    const int32_t space_count = description_width_pixels / nbsp_width_pixels + 1;
+
+    std::string spacing;
+
+    // spacing must use non breaking space as normal spaces are merged down to one by the text engine
+    for (int32_t i = 0; i < space_count; ++i) {
+        spacing += "\xC2\xA0";
+    }
+
+    unit_bonuses_text = spacing + unit_bonuses_text;
+
+    clan_select_menu_screen_text[0].title = description;
+
+    menu_draw_menu_title(window, &clan_select_menu_screen_text[0], COLOR_GREEN | GNW_TEXT_OUTLINE, false, false);
+
+    clan_select_menu_screen_text[0].title = unit_bonuses_text;
+    menu_draw_menu_title(window, &clan_select_menu_screen_text[0], 0xA2 | GNW_TEXT_OUTLINE, false, false);
 }
 
 void ClanSelectMenu::EventDone() {

src/clanselectmenu.hpp
@@ -84,7 +84,7 @@ class ClanSelectMenu {
 
     void ButtonInit(int32_t index, int32_t mode);
     void SelectMenuItems();
-    void DrawClanUpgrades(const char* text, int32_t index, int32_t color);
+    void DrawClanUpgrades();
 
 public:
     WindowInfo* window;

src/game_manager.cpp
@@ -1519,6 +1519,7 @@ bool GameManager_CargoSelection(uint16_t team) {
 
 bool GameManager_PlayerMissionSetup(uint16_t team) {
     int32_t team_gold;
+    auto mission_supplies = &UnitsManager_TeamMissionSupplies[team];
 
     GameManager_GameState = GAME_STATE_7_SITE_SELECT;
 
@@ -1534,17 +1535,40 @@ bool GameManager_PlayerMissionSetup(uint16_t team) {
         GameManager_ProcessTick(1);
     }
 
+    mission_supplies->units.Clear();
+    mission_supplies->cargos.Clear();
+
     do {
         GameManager_GameState = GAME_STATE_7_SITE_SELECT;
 
-        team_gold = ini_get_setting(INI_START_GOLD) + ini_clans.GetClanGold(UnitsManager_TeamInfo[team].team_clan);
+        auto clans = ResourceManager_GetClans();
+        auto clan_id = ResourceManager_GetClanID(static_cast<TeamClanType>(UnitsManager_TeamInfo[team].team_clan));
+
+        team_gold = ini_get_setting(INI_START_GOLD);
+
+        if (clans) {
+            team_gold += clans->GetCredits(clan_id);
+        }
 
         if (team_gold) {
             if (!GameManager_CargoSelection(team)) {
                 return false;
             }
+
         } else {
-            UnitsManager_AddDefaultMissionLoadout(team);
+            auto mission_manager = ResourceManager_GetMissionManager();
+
+            if (mission_manager && mission_manager->GetGameRulesHandler()) {
+                mission_supplies->team_gold = 0;
+                UnitsManager_TeamInfo[team].stats_gold_spent_on_upgrades = 0;
+
+                mission_supplies->units.Clear();
+                mission_supplies->cargos.Clear();
+
+                if (!mission_manager->GetGameRulesHandler()->GetMissionLoadout(team, clan_id, *mission_supplies)) {
+                    return false;
+                }
+            }
         }
 
         if (!GameManager_IsMapInitialized) {

commit b50a8fcfe9fc72ca2353fa670aedca701e59ee56

Fixed regression from 8316eb2.
- Units bought before backing out of landing zone selection could not be sold back after returning to the purchase menu.

src/cargomenu.cpp
@@ -49,10 +49,10 @@ CargoMenu::CargoMenu(uint16_t team) : AbstractUpgradeMenu(team, CARGOPIC) {
     cargos = UnitsManager_TeamMissionSupplies[team].cargos;
 
     if (unit_types2->GetCount()) {
-        // Restore previous session's gold amounts on re-entering cargo menu
+        // Restore previous session's gold amounts and loadout size on re-entering cargo menu
         start_gold = UnitsManager_TeamMissionSupplies[team].start_gold;
         team_gold = UnitsManager_TeamMissionSupplies[team].team_gold;
-        unit_count = UnitsManager_TeamMissionSupplies[team].units.GetCount();
+        unit_count = UnitsManager_TeamMissionSupplies[team].unit_count;
 
         for (int32_t i = UNIT_START; i < UNIT_END; ++i) {
             unitvalues_base[i] = UnitsManager_TeamInfo[team].team_units->GetBaseUnitValues(i);
@@ -80,6 +80,8 @@ CargoMenu::CargoMenu(uint16_t team) : AbstractUpgradeMenu(team, CARGOPIC) {
                 unit_count = UnitsManager_TeamMissionSupplies[team].units.GetCount();
             }
         }
+
+        UnitsManager_TeamMissionSupplies[team].unit_count = unit_count;
     }
 
     stats_background = new (std::nothrow) Image(11, 293, 250, 174);

src/teammissionsupplies.hpp
@@ -33,6 +33,8 @@ public:
     Point starting_position;
     uint16_t team_gold;
     uint16_t start_gold;
+    /// Size of the free mission loadout, which `units` no longer reveals once purchases are appended to it.
+    uint16_t unit_count;
     uint8_t proximity_alert_ack;
 
     TeamMissionSupplies();