klei1984/max

commit 4c85d646e4ac339effd30394c851cff9d2720301

Corrected various reimplementation issues.
- Unit upgrade costs were wrongly calculated.
- Saved file names were rendered in upper case.
- Infiltrators were unable to side step enemies.
- Enemies simply ran over infiltrators.
- Sprite indices were incorrectly defined as unsigned values.

src/upgradecontrol.cpp
@@ -23,7 +23,7 @@
 
 #include "sound_manager.hpp"
 
-int UpgradeControl_Factors[UPGRADE_CONTROL_COUNT] = {0, 4, 4, 8, 2, 2, 2, 8, 4, 16};
+int UpgradeControl_Factors[UPGRADE_CONTROL_COUNT] = {4, 4, 8, 2, 2, 2, 8, 4, 16};
 
 UpgradeControl::UpgradeControl(WinID window_id, int ulx, int uly, int button_right_r_value, int button_left_r_value,
                                unsigned short *team_gold)

src/abstractupgrademenu.cpp
@@ -245,8 +245,8 @@ void AbstractUpgradeMenu::AddUpgradeMilitary(ResourceID unit_type) {
         }
 
         if (base_unitvalues->GetAttribute(ATTRIB_AMMO)) {
-            AddUpgrade(UPGRADE_CONTROL_4, base_unitvalues->GetAttribute(ATTRIB_RANGE),
-                       base_attribs->GetAttribute(ATTRIB_RANGE), actual_attribs->GetAttributeAddress(ATTRIB_RANGE), 0);
+            AddUpgrade(UPGRADE_CONTROL_4, base_unitvalues->GetAttribute(ATTRIB_AMMO),
+                       base_attribs->GetAttribute(ATTRIB_AMMO), actual_attribs->GetAttributeAddress(ATTRIB_AMMO), 0);
         }
     }
 }

src/saveloadmenu.cpp
@@ -737,16 +737,16 @@ int SaveLoadMenu_MenuLoop(int is_saving_allowed, int is_text_mode) {
 
 void SaveLoadMenu_Save(const char *file_name, const char *save_name, bool play_voice) {
     SmartFileWriter file;
-    char file_path[PATH_MAX];
+    SmartString filepath;
+    SmartString filename(file_name);
     char team_types[PLAYER_TEAM_MAX - 1];
     struct SaveFormatHeader file_header;
     unsigned short game_state;
 
-    strcpy(file_path, ResourceManager_FilePathGameInstall);
-    ResourceManager_ToUpperCase(const_cast<char *>(file_name));
-    strcat(file_path, file_name);
+    filepath = ResourceManager_FilePathGameInstall;
+    filepath += filename.Toupper();
 
-    if (file.Open(file_path)) {
+    if (file.Open(filepath.GetCStr())) {
         GameManager_GuiSwitchTeam(GameManager_PlayerTeam);
     }
 

src/unitinfo.cpp
@@ -3668,7 +3668,7 @@ bool UnitInfo::AttemptSideStep(int grid_x, int grid_y, int angle) {
 
     if (orders == ORDER_AWAIT || orders == ORDER_SENTRY || orders == ORDER_MOVE) {
         if (speed > 0 || UnitsManager_TeamInfo[team].team_type == TEAM_TYPE_COMPUTER) {
-            if (this->grid_x == grid_x && this->grid_y == grid_y) {
+            if (this->grid_x != grid_x || this->grid_y != grid_y) {
                 int backup_grid_x = this->grid_x;
                 int backup_grid_y = this->grid_y;
 
@@ -3714,8 +3714,8 @@ bool UnitInfo::AttemptSideStep(int grid_x, int grid_y, int angle) {
                             }
 
                             if (step_cost) {
-                                if ((step_cost > speed * 4 + move_fraction) &&
-                                    UnitsManager_TeamInfo[team].team_type == TEAM_TYPE_COMPUTER) {
+                                if ((step_cost <= speed * 4 + move_fraction) ||
+                                    UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_COMPUTER) {
                                     best_angle = unit_angle;
                                     best_cost = step_cost;
                                     best_site = position;
@@ -3742,7 +3742,7 @@ bool UnitInfo::AttemptSideStep(int grid_x, int grid_y, int angle) {
 
                     SmartPointer<GroundPath> ground_path(new (std::nothrow) GroundPath(best_site.x, best_site.y));
 
-                    ground_path->AddStep(best_site.x - grid_x, best_site.y - grid_y);
+                    ground_path->AddStep(best_site.x - this->grid_x, best_site.y - this->grid_y);
 
                     path = &*ground_path;
 

src/unitinfo.hpp
@@ -249,12 +249,12 @@ public:
     unsigned char turret_angle;
     char turret_offset_x;
     char turret_offset_y;
-    unsigned short total_images;
-    unsigned short image_base;
-    unsigned short turret_image_base;
-    unsigned short firing_image_base;
-    unsigned short connector_image_base;
-    unsigned short image_index_max;
+    short total_images;
+    short image_base;
+    short turret_image_base;
+    short firing_image_base;
+    short connector_image_base;
+    short image_index_max;
     unsigned char orders;
     unsigned char state;
     unsigned char prior_orders;
@@ -317,8 +317,8 @@ public:
     unsigned char spotted_by_team[PLAYER_TEAM_MAX];
     Rect sprite_bounds;
     Rect shadow_bounds;
-    unsigned short image_index;
-    unsigned short turret_image_index;
+    short image_index;
+    short turret_image_index;
     Point shadow_offset;
     unsigned int field_221;
 };

src/upgradecontrol.hpp
@@ -23,21 +23,21 @@
 #define UPGRADECONTROL_HPP
 
 #include "button.hpp"
+#include "unitvalues.hpp"
 
 #define UPGRADECONTROL_UPGRADE_COST_LIMIT 800
 
 enum {
-    UPGRADE_CONTROL_0,
-    UPGRADE_CONTROL_1,
-    UPGRADE_CONTROL_2,
-    UPGRADE_CONTROL_3,
-    UPGRADE_CONTROL_4,
-    UPGRADE_CONTROL_5,
-    UPGRADE_CONTROL_6,
-    UPGRADE_CONTROL_7,
-    UPGRADE_CONTROL_8,
-    UPGRADE_CONTROL_9,
-    UPGRADE_CONTROL_COUNT
+    UPGRADE_CONTROL_1 = ATTRIB_ATTACK,
+    UPGRADE_CONTROL_2 = ATTRIB_ROUNDS,
+    UPGRADE_CONTROL_3 = ATTRIB_RANGE,
+    UPGRADE_CONTROL_4 = ATTRIB_AMMO,
+    UPGRADE_CONTROL_5 = ATTRIB_ARMOR,
+    UPGRADE_CONTROL_6 = ATTRIB_HITS,
+    UPGRADE_CONTROL_7 = ATTRIB_SCAN,
+    UPGRADE_CONTROL_8 = ATTRIB_SPEED,
+    UPGRADE_CONTROL_9 = ATTRIB_TURNS,
+    UPGRADE_CONTROL_COUNT = 9
 };
 
 class UpgradeControl {

commit a5000e359c5bee17825efa09f4d5bdd780c6dba5

Fixed regression from 4c85d64.
- Ammunition upgrades were priced eight times too high and hit the 800 credit ceiling almost at once, while speed upgrades were sold at half price.

src/upgradecontrol.cpp
@@ -25,7 +25,8 @@
 
 #include "sound_manager.hpp"
 
-int32_t UpgradeControl_Factors[UPGRADE_CONTROL_COUNT] = {4, 4, 8, 2, 2, 2, 8, 4, 16};
+/// Indexed by `ATTRIB_*`: attack, rounds, range, armor, hits, speed, scan, turns, ammo.
+int32_t UpgradeControl_Factors[UPGRADE_CONTROL_COUNT] = {4, 4, 8, 2, 2, 4, 8, 16, 2};
 
 UpgradeControl::UpgradeControl(WinID window_id, int32_t ulx, int32_t uly, int32_t button_right_r_value,
                                int32_t button_left_r_value, uint32_t* team_gold)