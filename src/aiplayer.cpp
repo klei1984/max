klei1984/max

commit 904f55f045e791f500ef0aad2bfb8f454564021a

Added ini parameter to configure level of computer cheating.
The M.A.X. v1.04 patch introduced several hacks to "Improve computer AI
in single player modes.". The new option allows users to disable those
hacks. The parameter is called cheating_computer and it is located in
the SETUP ini section.

Supported parameter values:
0 - All hacks are disabled.
1 - Expert and higher difficulty level computers learn location of all
mining station and eco-sphere on game start and game reload.
2 - In addition to the previous point, Master and higher difficulty
level computers learn location of all units at the beginning of each
turn. On all difficulty levels computers select mainly non computer
players to be their arch enemies.
3 - In addition to the previous point, normally computers only attack
players that are selected as their arch enemies.

src/aiplayer.cpp
@@ -818,7 +818,8 @@ ThreatMap* AiPlayer::GetThreatMap(int32_t risk_level, int32_t caution_level, boo
         }
 
         if (is_for_attacking) {
-            if (ini_get_setting(INI_OPPONENT) >= OPPONENT_TYPE_MASTER) {
+            if (ini_get_setting(INI_OPPONENT) >= OPPONENT_TYPE_MASTER &&
+                ini_get_setting(INI_CHEATING_COMPUTER) >= COMPUTER_CHEATING_LEVEL_SHAMELESS) {
                 for (SmartList<UnitInfo>::Iterator it = UnitsManager_StationaryUnits.Begin();
                      it != UnitsManager_StationaryUnits.End(); ++it) {
                     if (IsAbleToAttack(&*it, risk_group_unit, player_team) && (*it).team != player_team) {
@@ -1731,10 +1732,10 @@ void AiPlayer::CheckReconnaissanceNeeds(SmartObjectArray<BuildOrder>* build_orde
 SmartObjectArray<BuildOrder> AiPlayer::ChooseStrategicBuildOrders(bool mode) {
     SmartObjectArray<BuildOrder> build_orders;
 
-    if (target_team < PLAYER_TEAM_RED) {
+    if (!!IsTargetTeamDefined()) {
         for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
             if (team != player_team) {
-                if (target_team < PLAYER_TEAM_RED ||
+                if (!IsTargetTeamDefined() ||
                     (UnitsManager_TeamInfo[team].team_points > UnitsManager_TeamInfo[target_team].team_points &&
                      (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE &&
                       UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_ELIMINATED))) {
@@ -1744,7 +1745,7 @@ SmartObjectArray<BuildOrder> AiPlayer::ChooseStrategicBuildOrders(bool mode) {
         }
     }
 
-    if (target_team >= PLAYER_TEAM_RED) {
+    if (IsTargetTeamDefined()) {
         if (mode) {
             switch (strategy) {
                 case AI_STRATEGY_MISSILES:
@@ -2242,28 +2243,24 @@ void AiPlayer::AddMilitaryUnit(UnitInfo* unit) {
     }
 }
 
-void AiPlayer::BeginTurn() {
-    if (UnitsManager_TeamInfo[player_team].team_type == TEAM_TYPE_COMPUTER) {
-        InvalidateThreatMaps();
+bool AiPlayer::IsTargetTeamDefined() const { return (target_team >= PLAYER_TEAM_RED && target_team < PLAYER_TEAM_MAX); }
 
-        field_16 = 0;
+bool AiPlayer::IsTargetTeam(const uint8_t team) const {
+    bool result;
 
-        transport_orders.Clear();
+    if (ini_get_setting(INI_CHEATING_COMPUTER) >= COMPUTER_CHEATING_LEVEL_INSUFFERABLE) {
+        result = (team == target_team);
 
-        if (GameManager_PlayMode == PLAY_MODE_TURN_BASED) {
-            const char* team_colors[PLAYER_TEAM_MAX - 1] = {_(ba19), _(d486), _(ec7d), _(0d5a)};
-            char message[80];
+    } else {
+        result = (team != player_team) && (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE) &&
+                 (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_ELIMINATED);
+    }
 
-            sprintf(message, _(2ab8), team_colors[player_team]);
+    return result;
+}
 
-            MessageManager_DrawMessage(message, 0, 0);
-        }
-
-        Point position = GetTeamClusterCoordinates(player_team);
-
-        DetermineTargetLocation(position);
-        UpdatePriorityTasks();
-
+void AiPlayer::DetermineTargetTeam() {
+    if (ini_get_setting(INI_CHEATING_COMPUTER) >= COMPUTER_CHEATING_LEVEL_SHAMELESS) {
         int32_t player_team_points = UnitsManager_TeamInfo[player_team].team_points;
         int32_t team_building_counts[PLAYER_TEAM_MAX];
         int32_t target_team_points;
@@ -2276,7 +2273,7 @@ void AiPlayer::BeginTurn() {
             ++team_building_counts[(*it).team];
         }
 
-        if (target_team >= PLAYER_TEAM_RED) {
+        if (IsTargetTeamDefined()) {
             target_team_points = UnitsManager_TeamInfo[target_team].team_points;
 
             if (UnitsManager_TeamInfo[target_team].team_type != TEAM_TYPE_COMPUTER) {
@@ -2303,7 +2300,7 @@ void AiPlayer::BeginTurn() {
                     }
                 }
 
-                if (target_team < PLAYER_TEAM_RED || target_team_points < enemy_team_points ||
+                if (!IsTargetTeamDefined() || target_team_points < enemy_team_points ||
                     (target_team_points == enemy_team_points &&
                      team_building_counts[target_team] < team_building_counts[team])) {
                     target_team = team;
@@ -2311,6 +2308,45 @@ void AiPlayer::BeginTurn() {
                 }
             }
         }
+
+    } else {
+        for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
+            if (team != player_team) {
+                if (!IsTargetTeamDefined() ||
+                    (UnitsManager_TeamInfo[team].team_points > UnitsManager_TeamInfo[target_team].team_points &&
+                     (UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_NONE &&
+                      UnitsManager_TeamInfo[team].team_type != TEAM_TYPE_ELIMINATED))) {
+                    target_team = team;
+                }
+            }
+        }
+    }
+}
+
+void AiPlayer::BeginTurn() {
+    if (UnitsManager_TeamInfo[player_team].team_type == TEAM_TYPE_COMPUTER) {
+        InvalidateThreatMaps();
+
+        field_16 = 0;
+
+        transport_orders.Clear();
+
+        if (GameManager_PlayMode == PLAY_MODE_TURN_BASED) {
+            const char* team_colors[PLAYER_TEAM_MAX - 1] = {_(ba19), _(d486), _(ec7d), _(0d5a)};
+            char message[80];
+
+            sprintf(message, _(2ab8), team_colors[player_team]);
+
+            MessageManager_DrawMessage(message, 0, 0);
+        }
+
+        Point position = GetTeamClusterCoordinates(player_team);
+
+        DetermineTargetLocation(position);
+
+        UpdatePriorityTasks();
+
+        DetermineTargetTeam();
 
         if (need_init) {
             task_1 = new (std::nothrow) TaskCheckAssaults(player_team);
@@ -2484,7 +2520,8 @@ void AiPlayer::BeginTurn() {
                 }
             }
 
-            if (ini_get_setting(INI_OPPONENT) >= OPPONENT_TYPE_EXPERT) {
+            if (ini_get_setting(INI_OPPONENT) >= OPPONENT_TYPE_EXPERT &&
+                ini_get_setting(INI_CHEATING_COMPUTER) >= COMPUTER_CHEATING_LEVEL_TOLERABLE) {
                 for (SmartList<UnitInfo>::Iterator it = UnitsManager_StationaryUnits.Begin();
                      it != UnitsManager_StationaryUnits.End(); ++it) {
                     if ((*it).team != player_team &&
@@ -2495,7 +2532,7 @@ void AiPlayer::BeginTurn() {
             }
 
             for (SmartList<SpottedUnit>::Iterator it = spotted_units.Begin(); it != spotted_units.End(); ++it) {
-                if (IsKeyFacility((*it).GetUnit()->GetUnitType()) && (*it).GetUnit()->team == target_team) {
+                if (IsKeyFacility((*it).GetUnit()->GetUnitType()) && IsTargetTeam((*it).GetUnit()->team)) {
                     DetermineAttack(&*it, 0x1F00);
                 }
             }
@@ -2548,7 +2585,7 @@ void AiPlayer::BeginTurn() {
                 if (AiAttack_IsWithinReach(target, player_team, teams)) {
                     DetermineAttack(&*it, 0x1000);
 
-                } else if (target->team == target_team || target->team == PLAYER_TEAM_ALIEN) {
+                } else if (IsTargetTeam(target->team) || target->team == PLAYER_TEAM_ALIEN) {
                     if ((target->GetUnitType() == CONSTRCT && target->GetOrder() == ORDER_BUILD) ||
                         IsKeyFacility(target->GetUnitType()) ||
                         (target->team == PLAYER_TEAM_ALIEN &&
@@ -4041,11 +4078,11 @@ void AiPlayer::UnitSpotted(UnitInfo* unit) {
             }
 
             if ((IsKeyFacility(unit->GetUnitType()) || (!is_key_facility && unit->GetUnitType() == CONSTRCT)) &&
-                unit->team == target_team) {
+                IsTargetTeam(unit->team)) {
                 DetermineAttack(&*spotted_unit2, 0x1F00);
 
             } else if (!is_key_facility && unit->GetBaseValues()->GetAttribute(ATTRIB_ROUNDS) > 0) {
-                if (unit->team == target_team) {
+                if (IsTargetTeam(unit->team)) {
                     for (SmartList<UnitInfo>::Iterator it = UnitsManager_StationaryUnits.Begin();
                          it != UnitsManager_StationaryUnits.End(); ++it) {
                         if ((*it).team == unit->team && IsKeyFacility((*it).GetUnitType()) &&

assets/settings.ini
@@ -16,6 +16,7 @@ movie_play=0
 alt_movie_res=0
 language=english
 game_data=.
+cheating_computer=3
 
 [OPTIONS]
 world=0

src/aiplayer.hpp
@@ -123,7 +123,7 @@ class AiPlayer {
     bool IsDemoMode();
     void RegisterIdleUnits();
     static int32_t GetTotalProjectedDamage(UnitInfo* unit, int32_t caution_level, uint16_t team,
-                                       SmartList<UnitInfo>* units);
+                                           SmartList<UnitInfo>* units);
     static void UpdateMap(int16_t** map, Point position, int32_t range, int32_t damage_potential, bool normalize);
     static void UpdateThreatMaps(ThreatMap* threat_map, UnitInfo* unit, Point position, int32_t range, int32_t attack,
                                  int32_t shots, int32_t& ammo, bool normalize);
@@ -170,6 +170,9 @@ public:
     Task* FindManager(Point site);
     SmartList<SpottedUnit>& GetSpottedUnits();
     void AddMilitaryUnit(UnitInfo* unit);
+    bool IsTargetTeamDefined() const;
+    bool IsTargetTeam(const uint8_t team) const;
+    void DetermineTargetTeam();
     void BeginTurn();
     void GuessEnemyAttackDirections();
     void PlanMinefields();

src/enums.hpp
@@ -2295,6 +2295,13 @@ enum AiStrategyType : uint8_t {
     AI_STRATEGY_MAX
 };
 
+enum ComputerCheatingLevel : uint8_t {
+    COMPUTER_CHEATING_LEVEL_NONE,
+    COMPUTER_CHEATING_LEVEL_TOLERABLE,
+    COMPUTER_CHEATING_LEVEL_SHAMELESS,
+    COMPUTER_CHEATING_LEVEL_INSUFFERABLE
+};
+
 enum MapViewUpdateType : uint8_t {
     MAP_VIEW_ZOOM,
     MAP_VIEW_CENTER,

src/ini.hpp
@@ -100,6 +100,7 @@ enum IniParameter {
     INI_ALT_MOVIE_RES,
     INI_LANGUAGE,
     INI_GAME_DATA,
+	INI_CHEATING_COMPUTER,
 
     /// OPTIONS SECTION
     INI_OPTIONS,

src/inifile.cpp
@@ -118,6 +118,7 @@ static const IniKey ini_keys_table[] = {
     {"alt_movie_res", "0", INI_NUMERIC},
     {"language", "english", INI_STRING},
     {"game_data", "", INI_STRING},
+    {"cheating_computer", "3", INI_NUMERIC},
 
     /// Keep original layout for v1.04 save game compatibility
     {"OPTIONS", nullptr, INI_SECTION},

commit 434565b476e6a5a79e22b74a5289185f07f01c24

Fixed regression from 904f55f.
- Computer players never bought scan range upgrades at mission loadout for recon units.
- Computer players kept switching their arch enemies mid skirmish.

src/aiplayer.cpp
@@ -1723,7 +1723,7 @@ void AiPlayer::CheckReconnaissanceNeeds(SmartObjectArray<BuildOrder>* build_orde
 SmartObjectArray<BuildOrder> AiPlayer::ChooseStrategicBuildOrders(bool mode) {
     SmartObjectArray<BuildOrder> build_orders;
 
-    if (!!IsTargetTeamDefined()) {
+    if (!IsTargetTeamDefined()) {
         for (int32_t team = PLAYER_TEAM_RED; team < PLAYER_TEAM_MAX - 1; ++team) {
             if (team != player_team) {
                 if (!IsTargetTeamDefined() ||